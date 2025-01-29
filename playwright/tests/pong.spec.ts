0ce6c60268a41924c2c8f1cd75683e84e01760ab
Elias Tuhola <elias.tuhola@gmail.com>
2025-01-29 10:08:22 +0200

    Changed test name
    Co-Authored-By: Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>

--- playwright/tests/pong.spec.ts
+++ playwright/tests/pong.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 const url = "http://localhost:5173";
 
-test("has title", async ({ page }) => {
+test("contains pong text", async ({ page }) => {
   await page.goto(url);
 
   await expect(page.getByText("pong")).toBeVisible();
