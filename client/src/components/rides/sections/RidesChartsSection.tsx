e620867a739a318bd90f2ef6a0a4fccd5cba3f09
Elias Tuhola <elias.tuhola@gmail.com>
2025-01-31 12:06:51 +0200

    Showing 4 charts

---
+++ client/src/components/rides/sections/RidesChartsSection.tsx
@@ -0,0 +1,31 @@
+import { Box, Container, Grid2 } from "@mui/material";
+import { RidesChartGridItem } from "../charts-grid-item/RidesChartGridItem";
+import { PaymentMethodDistributionChart } from "../charts/PaymentMethodDistributionChart";
+import { RidesPerWeekdayBarChart } from "../charts/RidesWeekdayDistributionChart";
+import { VehicleTypeDistributionChart } from "../charts/VehicleTypeDistributionChart";
+import { FarePerDayOfWeekChart } from "../charts/FarePerDayOfWeekChart";
+
+export const RidesChartsSection = () => {
+  return (
+    <Container maxWidth={false}>
+      <Grid2 container spacing={2}>
+        <RidesChartGridItem
+          title="Rides per day of week"
+          chart={<RidesPerWeekdayBarChart />}
+        />
+        <RidesChartGridItem
+          title="Fare per day of week"
+          chart={<FarePerDayOfWeekChart />}
+        />
+        <RidesChartGridItem
+          title="Vehicle type distribution"
+          chart={<VehicleTypeDistributionChart />}
+        />
+        <RidesChartGridItem
+          title="Payment method distribution"
+          chart={<PaymentMethodDistributionChart />}
+        />
+      </Grid2>
+    </Container>
+  );
+};
