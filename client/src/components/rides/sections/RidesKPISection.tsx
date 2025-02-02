ed35d11c33dd24bbfbbe6f607847a9011c6ab8dd
tuheli <94482031+tuheli@users.noreply.github.com>
2025-02-02 12:28:49 +0200

    Removed container elements

--- client/src/components/rides/sections/RidesKPISection.tsx
+++ client/src/components/rides/sections/RidesKPISection.tsx
@@ -1,11 +1,4 @@
-import {
-  Container,
-  Divider,
-  Grid2,
-  Paper,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Divider, Grid2, Paper, Stack, Typography } from "@mui/material";
 import { getKPIData } from "../../../data/rideData";
 import { KPIItem } from "../key-performance-indicators/KPIItem";
 
@@ -13,31 +6,24 @@ export const RidesKPISection = () => {
   const kpiData = getKPIData();
 
   return (
-    <Container
-      maxWidth={false}
-      sx={{
-        my: 2,
-      }}
-    >
-      <Paper>
-        <Stack>
-          <Typography
-            sx={{
-              px: 2,
-              py: 2,
-              fontWeight: 500,
-            }}
-          >
-            Key Performance Indicators
-          </Typography>
-          <Divider />
-          <Grid2 container spacing={2} sx={{ p: 2 }}>
-            {kpiData.map((kpi) => (
-              <KPIItem key={kpi.label} label={kpi.label} value={kpi.value} />
-            ))}
-          </Grid2>
-        </Stack>
-      </Paper>
-    </Container>
+    <Paper>
+      <Stack>
+        <Typography
+          sx={{
+            px: 2,
+            py: 2,
+            fontWeight: 500,
+          }}
+        >
+          Key Performance Indicators
+        </Typography>
+        <Divider />
+        <Grid2 container spacing={2} sx={{ p: 2 }}>
+          {kpiData.map((kpi) => (
+            <KPIItem key={kpi.label} label={kpi.label} value={kpi.value} />
+          ))}
+        </Grid2>
+      </Stack>
+    </Paper>
   );
 };
