a1d8273e7f9ed4b4e6b87067c06294cc3daf5704
tuheli <94482031+tuheli@users.noreply.github.com>
2025-02-02 12:29:25 +0200

    Added top bar and modified page layout

--- client/src/components/pages/DashboardPage.tsx
+++ client/src/components/pages/DashboardPage.tsx
@@ -1,12 +1,20 @@
-import { Stack } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { RidesKPISection } from "../rides/sections/RidesKPISection";
 import { RidesChartsSection } from "../rides/sections/RidesChartsSection";
+import { TopBar } from "../top-bar/TopBar";
 
 export const DashboardPage = () => {
   return (
-    <Stack>
-      <RidesKPISection />
-      <RidesChartsSection />
-    </Stack>
+    <>
+      <TopBar />
+      <Container maxWidth="xl">
+        <Box mt={2}>
+          <RidesKPISection />
+        </Box>
+        <Box mt={2}>
+          <RidesChartsSection />
+        </Box>
+      </Container>
+    </>
   );
 };
