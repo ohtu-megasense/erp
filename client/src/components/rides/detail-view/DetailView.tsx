7591a9c71adc585ca207eb91883513a2cc956abc
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-05 14:10:13 +0200

    Updated detail view layout

--- client/src/components/rides/detail-view/DetailView.tsx
+++ client/src/components/rides/detail-view/DetailView.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { RideData } from "../../../data/rideData";
 import { getPickupDateString } from "../../../utils/utils";
 import { AccordionWrapper } from "./AccordionWrapper";
@@ -11,21 +11,23 @@ interface DetailViewProps {
 export const DetailView = ({ rideDataArray }: DetailViewProps) => {
   return (
     <Box>
-      <Typography sx={{ p: 2 }}>
+      <Typography sx={{ py: 2 }}>
         Found {rideDataArray.length}{" "}
         {rideDataArray.length === 1 ? "ride" : "rides"}
       </Typography>
-      {rideDataArray.map((rideData) => (
-        <AccordionWrapper
-          title={`Ride ${rideData.rideId} - ${getPickupDateString(
-            rideData.requestTime
-          )}`}
-          key={rideData.rideId}
-          defaultExpanded={rideDataArray.length === 1}
-        >
-          <RideDataFullDetails key={rideData.rideId} rideData={rideData} />
-        </AccordionWrapper>
-      ))}
+      <Stack gap={2}>
+        {rideDataArray.map((rideData) => (
+          <AccordionWrapper
+            title={`Ride ${rideData.rideId} - ${getPickupDateString(
+              rideData.requestTime
+            )}`}
+            key={rideData.rideId}
+            defaultExpanded={rideDataArray.length === 1}
+          >
+            <RideDataFullDetails key={rideData.rideId} rideData={rideData} />
+          </AccordionWrapper>
+        ))}
+      </Stack>
     </Box>
   );
 };
