5c9fa867e012fe3bd90ffef79194f057c359da6a
Juho <juholukka00@gmail.com>
2025-02-05 10:04:08 +0200

    prettier&eslint on server too

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -7,16 +7,16 @@ import {
 import { LinkProps, PaletteMode } from "@mui/material";
 import { LinkBehavior } from "./LinkBehavior";
 
-declare module "@mui/material/styles/createPalette" {
+declare module '@mui/material/styles/createPalette' {
   interface Palette {
     vars: {
-      "mui-drawer-width": number;
+      'mui-drawer-width': number;
     };
   }
 
   interface PaletteOptions {
     vars?: {
-      "mui-drawer-width": number;
+      'mui-drawer-width': number;
     };
   }
 }
@@ -25,27 +25,27 @@ const lightPalette: PaletteOptions = {};
 
 const darkPalette: PaletteOptions = {
   background: {
-    default: "#2a2a2aff",
-    paper: "#393939",
+    default: '#2a2a2aff',
+    paper: '#393939'
   },
   text: {
-    primary: "#fcfcfc",
-    secondary: "#fcfcfc",
+    primary: '#fcfcfc',
+    secondary: '#fcfcfc'
   },
   primary: {
-    main: "#55009bff",
+    main: '#55009bff'
   },
-  divider: "#79797977",
+  divider: '#79797977'
 };
 
 const getPalette = (mode: PaletteMode): PaletteOptions => {
   switch (mode) {
-    case "light":
+    case 'light':
       return lightPalette;
-    case "dark":
+    case 'dark':
       return darkPalette;
     default:
-      throw new Error("Invalid palette mode.");
+      throw new Error('Invalid palette mode.');
   }
 };
 
@@ -55,15 +55,15 @@ export const getTheme = (mode: PaletteMode): Theme => {
 
   const palette: PaletteOptions = {
     ...defaultPalette,
-    ...customPalette,
+    ...customPalette
   };
 
   const themeOptions: ThemeOptions = {
     palette: {
       ...palette,
       vars: {
-        "mui-drawer-width": 300,
-      },
+        'mui-drawer-width': 300
+      }
     },
     typography: {},
     components: {
