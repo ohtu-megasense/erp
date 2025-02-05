7ccd20dc7f843a4af66cbc78defa7112e72ed8c9
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2025-02-05 12:31:27 +0200

    Update database schema

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,7 +1,15 @@
-CREATE TABLE inventory(
+CREATE TABLE items(
     id SERIAL PRIMARY KEY,
-    item_name TEXT,
-    amount INT,
-    item_location TEXT,
-    cost INT
+    item_name TEXT
+);
+
+CREATE TABLE value_types(
+    value_id SERIAL PRIMARY KEY,
+    value_name TEXT,
+    value_type TEXT
+);
+
+CREATE TABLE values(
+    item_id INT REFERENCES items(id),
+    value_id INT REFERENCES value_types(value_id)
 );
