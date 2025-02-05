2ee4be585c8ef7ebb2ed6dad55fcbe0cdbe25715
Santeri <santeri02.kuusela@gmail.com>
2025-02-05 12:36:25 +0200

    fixed database.ts so it initializes the database

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,29 +1,34 @@
-const { Client } = require("pg");
+import { Client } from "pg";
 import { database_URL } from "../config";
 import fs from "fs";
 import path from "path";
 
+const caPath = path.join(__dirname, "../../ca.pem");
+
+const client = new Client({
+  connectionString: database_URL,
+  ssl: {
+    ca: fs.readFileSync(caPath).toString(),
+  },
+});
+
 const loadSchema = async () => {
-    const client = new Client({
-        connectionString: database_URL,
-    })
+  try {
+    await client.connect();
+    console.log("Connected to the database");
 
-    try {
-        await client.connect();
-        console.log("Connected to the database");
+    const schemaPath = path.join(__dirname, "schema.sql");
+    const SQLschema = fs.readFileSync(schemaPath, "utf-8");
 
-        const schemaPath = path.join(__dirname, "schema.sql");
-        const SQLschema = fs.readFileSync(schemaPath, "utf-8");
+    await client.query(SQLschema);
+    console.log("Schema loaded successfully");
+  } finally {
+    await client.end();
+    console.log("Disconnected from the database");
+  }
+};
 
-        await client.query(SQLschema);
-        console.log("Schema loaded successfully");  
-    }finally{
-        await client.end();
-        console.log("Disconnected from the database");
-    }
-    
-}
-if (require.main == module){
-   loadSchema(); 
+if (require.main == module) {
+  loadSchema();
 }
 export { loadSchema };
