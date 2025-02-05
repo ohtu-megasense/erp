1ecb20ffe836fa4fd2556406788d6fe9ff63d329
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-05 13:45:37 +0200

    Moved layout out of dashboard page component

--- client/src/components/pages/dashboard/DashboardPage.tsx
+++ client/src/components/pages/dashboard/DashboardPage.tsx
@@ -1,34 +1,19 @@
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
 import { RidesKPISection } from "../../rides/sections/RidesKPISection";
 import { RidesChartsSection } from "../../rides/sections/RidesChartsSection";
-import { TopBar } from "../../top-bar/TopBar";
-import { Drawer } from "../../drawer/Drawer";
 import { HeadingSection } from "./HeadingSection";
 
 export const DashboardPage = () => {
   return (
     <>
-      <TopBar />
-      <Box display="flex">
-        <Drawer />
-        <Container
-          maxWidth="xl"
-          sx={{
-            mb: 2,
-          }}
-        >
-          <Box component="main">
-            <Box mt={2}>
-              <HeadingSection />
-            </Box>
-            <Box mt={2}>
-              <RidesKPISection />
-            </Box>
-            <Box mt={2}>
-              <RidesChartsSection />
-            </Box>
-          </Box>
-        </Container>
+      <Box mt={2}>
+        <HeadingSection />
+      </Box>
+      <Box mt={2}>
+        <RidesKPISection />
+      </Box>
+      <Box mt={2}>
+        <RidesChartsSection />
       </Box>
     </>
   );
