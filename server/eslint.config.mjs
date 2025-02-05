b05f91ef486ce6d14a07efb119ee051fafd86d43
Juho <juholukka00@gmail.com>
2025-02-05 10:34:03 +0200

    fix server eslint

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -5,18 +5,8 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"]
-  },
-  {
-    files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {
-  languageOptions: {
-    globals: {
-      ...globals.browser 
-    }
-  }
-  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
