7f04799ff4741600e3b4b05f37a196f40f5535ed
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-05 13:50:13 +0200

    Added detail view page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "./components/theme-provider/ThemeProvider";
 import { DashboardPage } from "./components/pages/dashboard/DashboardPage";
+import { DetailViewPage } from "./components/pages/detail-view/DetailViewPage";
+import { Route, Routes } from "react-router";
+import { DefaultPageLayout } from "./components/pages/layout/DefaultPageLayout";
+import { Redirect } from "./components/redirect/Redirect";
 
 export const App = () => {
   return (
     <ThemeProvider>
       <CssBaseline />
-      <DashboardPage />
+      <Routes>
+        <Route element={<DefaultPageLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/search" element={<DetailViewPage />} />
+        </Route>
+        <Route path="*" element={<Redirect to="/" />} />
+      </Routes>
     </ThemeProvider>
   );
 };
