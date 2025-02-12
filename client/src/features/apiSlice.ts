abc8be63b19516f23ac66f20370baa691ca23386
Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>
2025-02-12 13:35:56 +0200

    add API query for fetching inventory report data

--- client/src/features/apiSlice.ts
+++ client/src/features/apiSlice.ts
@@ -4,6 +4,14 @@ interface PingResponse {
   message: string;
 }
 
+interface InventoryReport {
+  total_sensors: number;
+  active_sensors: number;
+  inactive_sensors: number;
+  total_cloud_resources: number;
+  monthly_api_usage: number;
+}
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
@@ -12,8 +20,11 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getPing: builder.query<PingResponse, void>({
       query: () => 'ping'
+    }),
+    getInventory: builder.query<InventoryReport, void>({
+      query: () => 'reports/inventory'
     })
   })
 });
 
-export const { useGetPingQuery } = apiSlice;
+export const { useGetPingQuery, useGetInventoryQuery } = apiSlice;
