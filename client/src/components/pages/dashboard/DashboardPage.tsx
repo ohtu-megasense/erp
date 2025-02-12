3030e58c5e66c678e0ecd65575c84cfeda3f2c8a
Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>
2025-02-12 15:12:54 +0200

    inventory data handling to InventoryDataSection

--- client/src/components/pages/dashboard/DashboardPage.tsx
+++ client/src/components/pages/dashboard/DashboardPage.tsx
@@ -1,7 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { RidesKPISection } from "../../rides/sections/RidesKPISection";
 import { RidesChartsSection } from "../../rides/sections/RidesChartsSection";
 import { HeadingSection } from "./HeadingSection";
+import { InventoryDataSection } from "../../rides/sections/InventoryDataSection";
+
 
 export const DashboardPage = () => {
   return (
@@ -14,6 +16,9 @@ export const DashboardPage = () => {
       </Box>
       <Box mt={2}>
         <RidesChartsSection />
+      </Box>
+      <Box mt={2}>
+        <InventoryDataSection />
       </Box>
     </>
   );
