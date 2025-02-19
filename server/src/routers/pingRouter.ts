252203bc4517cce3ebe2916492b712ce52c73968
Santeri <santeri02.kuusela@gmail.com>
2025-02-19 09:48:13 +0200

    inventory router not ready

--- server/src/routers/pingRouter.ts
+++ server/src/routers/pingRouter.ts
@@ -4,6 +4,10 @@ interface PingResponse {
   message: string;
 }
 
+interface InventoryModule {
+  message: string;
+}
+
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -13,5 +17,15 @@ router.get('/', (req, res) => {
 
   res.json(response);
 });
+
+router.get("/inventorymodule", (req, res) => {
+  const response: InventoryModule = {
+    message: "testi"
+  }
+
+  res.json(response);
+});
+
+
 
 export default router;
