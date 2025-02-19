9943dbc1996457a2f7bb02e7b50620bd0583476c
Jasse <jasse.merivirta@helsinki.fi>
2025-02-19 16:04:26 +0200

    remove cert from database_handler

--- server/src/database/database_handler.ts
+++ server/src/database/database_handler.ts
@@ -1,158 +1,145 @@
-import { Pool } from "pg";
-import format from "pg-format";
-import { database_URL } from "../config";
-import fs from "fs";
-import path from "path";
-
-const caPath = path.join(__dirname, "../../development_certificate.pem");
+import { Pool } from 'pg';
+import format from 'pg-format';
+import { database_URL } from '../config';
 
 const pool = new Pool({
-	connectionString: database_URL,
-
-	ssl: {
-		ca: fs.readFileSync(caPath).toString(),
-	},
+  connectionString: database_URL
 });
 
 export const createInventoryItem = async (
-	tableName: string,
-	columns: Array<string>,
+  tableName: string,
+  columns: Array<string>
 ) => {
-	const client = await pool.connect();
-	try {
-		console.log("Connected to the database");
+  const client = await pool.connect();
+  try {
+    console.log('Connected to the database');
 
-		let sql_text: string = "CREATE TABLE %I (id SERIAL PRIMARY KEY, ";
-		for (var col of columns) {
-			sql_text = sql_text + "%I TEXT, ";
-		}
-		sql_text = sql_text.slice(0, -2); // this removes the extra , and space
-		sql_text = sql_text + ");";
+    let sql_text: string = 'CREATE TABLE %I (id SERIAL PRIMARY KEY, ';
+    for (var col of columns) {
+      sql_text = sql_text + '%I TEXT, ';
+    }
+    sql_text = sql_text.slice(0, -2); // this removes the extra , and space
+    sql_text = sql_text + ');';
 
-		console.log(sql_text);
-		console.log(tableName);
-		console.log(columns);
+    console.log(sql_text);
+    console.log(tableName);
+    console.log(columns);
 
-		const query = format(sql_text, tableName, ...columns);
+    const query = format(sql_text, tableName, ...columns);
 
-		await client.query(query);
-		console.log(
-			`Table "${tableName}" with columns ${columns} created successfully!`,
-		);
-	} catch (error) {
-		console.error("Error creating table:", error);
-	} finally {
-		client.release();
-		console.log("Disconnected from the database");
-	}
+    await client.query(query);
+    console.log(
+      `Table "${tableName}" with columns ${columns} created successfully!`
+    );
+  } catch (error) {
+    console.error('Error creating table:', error);
+  } finally {
+    client.release();
+    console.log('Disconnected from the database');
+  }
 };
 
 export const addToInventoryItem = async (
-	tableName: string,
-	values: Array<string>,
+  tableName: string,
+  values: Array<string>
 ) => {
-	const client = await pool.connect();
-	try {
-		console.log("Connected to the database");
+  const client = await pool.connect();
+  try {
+    console.log('Connected to the database');
 
-		let sql_text: string = 'INSERT INTO "%I" VALUES (DEFAULT, ';
-		for (var val of values) {
-			sql_text = sql_text + "'%s', ";
-		}
-		sql_text = sql_text.slice(0, -2); // this removes the extra , and space
-		sql_text = sql_text + ");";
+    let sql_text: string = 'INSERT INTO "%I" VALUES (DEFAULT, ';
+    for (var val of values) {
+      sql_text = sql_text + "'%s', ";
+    }
+    sql_text = sql_text.slice(0, -2); // this removes the extra , and space
+    sql_text = sql_text + ');';
 
-		console.log(sql_text);
-		console.log(tableName);
-		console.log(values);
+    console.log(sql_text);
+    console.log(tableName);
+    console.log(values);
 
-		const query = format(sql_text, tableName, ...values);
+    const query = format(sql_text, tableName, ...values);
 
-		await client.query(query);
-		console.log(`Table "${tableName}" added values ${values}!`);
-	} catch (error) {
-		console.error("Error adding values:", error);
-	} finally {
-		client.release();
-		console.log("Disconnected from the database");
-	}
+    await client.query(query);
+    console.log(`Table "${tableName}" added values ${values}!`);
+  } catch (error) {
+    console.error('Error adding values:', error);
+  } finally {
+    client.release();
+    console.log('Disconnected from the database');
+  }
 };
 
 export async function retrieveInventoryTable(tableName: string) {
-	const client = await pool.connect();
-	try {
-		console.log("Connected to the database");
+  const client = await pool.connect();
+  try {
+    console.log('Connected to the database');
 
-		let sql_text: string = 'SELECT * FROM "%I"';
-		console.log(sql_text);
-		console.log(tableName);
+    let sql_text: string = 'SELECT * FROM "%I"';
+    console.log(sql_text);
+    console.log(tableName);
 
-		const query = format(sql_text, tableName);
-		console.log(query);
-		const result = await client.query(query);
-		console.log(`Retrieved everything from "${tableName}"!`);
+    const query = format(sql_text, tableName);
+    console.log(query);
+    const result = await client.query(query);
+    console.log(`Retrieved everything from "${tableName}"!`);
 
-		const results = result.rows;
-		return results;
-
-	} catch (error) {
-		console.error("Error retrieving from table:", error);
-	} finally {
-		client.release();
-		console.log("Disconnected from the database");
-	}
+    const results = result.rows;
+    return results;
+  } catch (error) {
+    console.error('Error retrieving from table:', error);
+  } finally {
+    client.release();
+    console.log('Disconnected from the database');
+  }
 }
-
 
 export async function temporarySensorKoosteFunction() {
-	const client = await pool.connect();
-	try {
-		console.log("Connected to the database");
+  const client = await pool.connect();
+  try {
+    console.log('Connected to the database');
 
-		let sql_text: string = "SELECT COUNT(status) FROM sensors WHERE status='Active'";
-		const query = format(sql_text);
-		const result_active = await client.query(query);
-		console.log(`Retrieved count from "sensors"!`);
-		const results_active = result_active.rows;
+    let sql_text: string =
+      "SELECT COUNT(status) FROM sensors WHERE status='Active'";
+    const query = format(sql_text);
+    const result_active = await client.query(query);
+    console.log(`Retrieved count from "sensors"!`);
+    const results_active = result_active.rows;
 
-    let sql_text_all: string = "SELECT COUNT(status) FROM sensors";
-		const query_2 = format(sql_text_all);
-		const result_all = await client.query(query_2);
-		console.log(`Retrieved count from "sensors"!`);
-		const results_all = result_all.rows;
+    let sql_text_all: string = 'SELECT COUNT(status) FROM sensors';
+    const query_2 = format(sql_text_all);
+    const result_all = await client.query(query_2);
+    console.log(`Retrieved count from "sensors"!`);
+    const results_all = result_all.rows;
 
-		return {
-      total_sensors: results_all[0]["count"],
-      active_sensors: results_active[0]["count"],
-      inactive_sensors: results_all[0]["count"]-results_active[0]["count"]
+    return {
+      total_sensors: results_all[0]['count'],
+      active_sensors: results_active[0]['count'],
+      inactive_sensors: results_all[0]['count'] - results_active[0]['count']
     };
-
-	} catch (error) {
-		console.error("Error retrieving from table:", error);
-	} finally {
-		client.release();
-		console.log("Disconnected from the database");
-	}
+  } catch (error) {
+    console.error('Error retrieving from table:', error);
+  } finally {
+    client.release();
+    console.log('Disconnected from the database');
+  }
 }
 
-
-
-
 if (require.main == module) {
-  temporarySensorKoosteFunction()
+  temporarySensorKoosteFunction();
   /**
-	createInventoryItem("sensors", [
-		"name",
-		"location",
-		"status",
-		"last_updated",
-	]);
+  createInventoryItem("sensors", [
+    "name",
+    "location",
+    "status",
+    "last_updated",
+  ]);
   */
- 
+
   //createInventoryItem('app_metrics', ["app_name", "platform", "downloads", "app_rating", "active_subscriptions", "revenue", "last_updated"])
   //addToInventoryItem('app_metrics', ['GO2-app', 'iOS', "892", "3.9", "108", "1080"]);
   //retrieveInventoryTable("app_metrics")
-	//addToInventoryItem("lusers", ["santeri", "kuusela"]);
+  //addToInventoryItem("lusers", ["santeri", "kuusela"]);
 }
 
 //export default addToInventoryItem;
