a31874d12a308f631a58407123425d4a2c6791ea
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-26 12:11:41 +0200

    Added category modal component to default page layout

--- client/src/components/pages/layout/DefaultPageLayout.tsx
+++ client/src/components/pages/layout/DefaultPageLayout.tsx
@@ -1,18 +1,20 @@
-import { Box, Container } from "@mui/material";
-import { TopBar } from "../../top-bar/TopBar";
-import { Drawer } from "../../drawer/Drawer";
-import { Outlet } from "react-router";
+import { Box, Container } from '@mui/material';
+import { TopBar } from '../../top-bar/TopBar';
+import { Drawer } from '../../drawer/Drawer';
+import { Outlet } from 'react-router';
+import { AddCategoryModal } from '../../categories/AddCategoryModal';
 
 export const DefaultPageLayout = () => {
   return (
     <>
+      <AddCategoryModal />
       <TopBar />
       <Box display="flex">
         <Drawer />
         <Container
           maxWidth="xl"
           sx={{
-            mb: 2,
+            mb: 2
           }}
         >
           <Box component="main">
