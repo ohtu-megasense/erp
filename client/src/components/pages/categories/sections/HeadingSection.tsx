b87aab088968c6f60270fa4a14d465aba678fba1
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-28 12:38:29 +0200

    Create HeadingSection.tsx

---
+++ client/src/components/pages/categories/sections/HeadingSection.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+
+export const HeadingSection = () => {
+  return (
+    <>
+      <Typography variant="h4">Categories</Typography>
+      <Typography variant="caption" ml={0.5}>
+        Listing of categories and actions to modify them
+      </Typography>
+    </>
+  );
+};
