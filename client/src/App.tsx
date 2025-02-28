55490ecbfc5e9c4318cef7beafa3b9a90d0d793d
Elias Tuhola <elias.tuhola@gmail.com>
2025-02-28 12:39:34 +0200

    Added categories page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "./components/theme-provider/ThemeProvider";
-import { DashboardPage } from "./components/pages/dashboard/DashboardPage";
-import { DetailViewPage } from "./components/pages/detail-view/DetailViewPage";
-import { Route, Routes } from "react-router";
-import { DefaultPageLayout } from "./components/pages/layout/DefaultPageLayout";
-import { Redirect } from "./components/redirect/Redirect";
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from './components/theme-provider/ThemeProvider';
+import { DashboardPage } from './components/pages/dashboard/DashboardPage';
+import { DetailViewPage } from './components/pages/detail-view/DetailViewPage';
+import { Route, Routes } from 'react-router';
+import { DefaultPageLayout } from './components/pages/layout/DefaultPageLayout';
+import { Redirect } from './components/redirect/Redirect';
+import { CategoriesPage } from './components/pages/categories/CategoriesPage';
 
 export const App = () => {
   return (
@@ -13,6 +14,7 @@ export const App = () => {
       <Routes>
         <Route element={<DefaultPageLayout />}>
           <Route path="/" element={<DashboardPage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
           <Route path="/search" element={<DetailViewPage />} />
         </Route>
         <Route path="*" element={<Redirect to="/" />} />
