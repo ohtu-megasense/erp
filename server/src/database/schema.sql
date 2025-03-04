ce7d3b1f67150fb64c37845544d5bbae979b8b99
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-03-04 14:40:45 +0200

    changes to schema for adding the new categories and item types

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,28 +1,14 @@
+DROP TABLE item;
+DROP TABLE category;
 
-
-/*
-CREATE TABLE IF NOT EXISTS app_metrics (
+CREATE TABLE IF NOT EXISTS category (
     id SERIAL PRIMARY KEY,
-    app_name VARCHAR(50),
-    platform VARCHAR(10), -- 'iOS' or 'Android'
-    downloads INT DEFAULT 0,
-    app_rating DECIMAL(3, 2),
-    active_subscriptions INT DEFAULT 0,
-    revenue DECIMAL(10,2),
-    last_updated TIMESTAMP DEFAULT NOW()
+    item_name TEXT NOT NULL,
+    item_shape JSON,
+    item JSON[]
 );
 
-CREATE TABLE IF NOT EXISTS field_sensors (
-    id SERIAL PRIMARY KEY,
-    sensor_name VARCHAR(50),
-    sensor_location VARCHAR(50),
-    sensor_status VARCHAR(20),
-    last_updated TIMESTAMP DEFAULT NOW()
-)
-*/
-
-
-CREATE TABLE inventory_module (
-    id SERIAL PRIMARY KEY,
-    inventory_item VARCHAR(50)
-)
+CREATE TABLE IF NOT EXISTS item (
+    id INT PRIMARY KEY REFERENCES category (id),
+    item_data JSON
+);
