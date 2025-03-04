428c2c80281af0169c4b0b5caed61379c4d2e6ef
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-04 14:55:52 +0200

    Removed tables from visualize page

--- client/src/components/pages/categories/visualize/VisualizeCategoriesPage.tsx
+++ client/src/components/pages/categories/visualize/VisualizeCategoriesPage.tsx
@@ -1,15 +1,11 @@
 import { Box } from '@mui/material';
 import { HeadingSection } from './HeadingSection';
-import { CategoryTablesSection } from '../../../categories/visualize/CategoryTablesSection';
 
 export const VisualizeCategoriesPage = () => {
   return (
     <>
       <Box mt={2}>
         <HeadingSection />
-      </Box>
-      <Box mt={4}>
-        <CategoryTablesSection />
       </Box>
     </>
   );
