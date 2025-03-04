82838512aa0f13fc04a6d8aee9a6e39778135d82
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-04 18:31:46 +0200

    Included add category section to page

--- client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
+++ client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
@@ -1,12 +1,16 @@
 import { Box } from '@mui/material';
 import { HeadingSection } from './HeadingSection';
 import { ManageCategoriesSection } from '../../../categories/manage/ManageCategoriesSection';
+import { AddCategorySection } from '../../../categories/manage/AddCategorySection';
 
 export const ManageCategoriesPage = () => {
   return (
     <>
       <Box mt={2}>
         <HeadingSection />
+      </Box>
+      <Box mt={2}>
+        <AddCategorySection />
       </Box>
       <Box mt={4}>
         <ManageCategoriesSection />
