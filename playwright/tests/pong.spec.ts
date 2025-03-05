6ed16f3072f5187d200dd69981685ae110e65794
Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>
2025-03-05 16:33:59 +0200

    add 2 values test works

--- playwright/tests/pong.spec.ts
+++ playwright/tests/pong.spec.ts
@@ -29,3 +29,31 @@ test("create new category with 2 properties", async ({ page }) => {
 
   await expect(page.getByText("New category created")).toBeVisible();
 });
+
+test("add 2 values", async ({ page }) => {
+  
+  //navigate to  manage categories
+  await page.goto(url);
+  await page.click("text=Categories");
+  await page.click("text=Manage");
+  
+  // create new category with two properites
+  await page.locator("input").first().fill("New category");
+  await page.click("text=Add property");
+  await page.locator("input").nth(1).fill("Property1");
+  await page.click("text=Add property");
+  await page.locator("input").nth(2).fill("Property2");
+  await page.click("button:has-text('Create')");
+
+  // add two values
+  const newCategorySection = page.locator("text=New Category").locator("..");
+  await newCategorySection.locator("[data-testid='AddIcon']").locator("..").click();
+  await newCategorySection.locator("input").nth(0).fill("Value1");
+  await newCategorySection.locator("input").nth(1).fill("Value2");
+  await newCategorySection.locator("button:has-text('+ ADD')").click();
+
+  //check that two values are added
+  await expect(newCategorySection.getByText("Value1")).toBeVisible();
+  await expect(newCategorySection.getByText("Value2")).toBeVisible();
+});
+
