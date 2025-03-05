24574d6c86f007d58bf02a0d1912fa3592a84d66
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-03-05 09:48:25 +0200

    changes
    Co-authored-by: Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>

--- server/src/routers/categoryRouter.ts
+++ server/src/routers/categoryRouter.ts
@@ -5,13 +5,15 @@ const router = Router();
 
 router.post("/", (req, res) => {
 	const { name, ...rest } = req.body; // extracting name from request body as string
+	console.log(rest);
+
 	if (!name) {
 		console.error("Category needs name");
 		res.status(500).json({ error: "Category needs a name" });
 		return;
 	}
 
-	AddingCategoryFunction(name)
+	AddingCategoryFunction(name, rest)
 		.then((data) => res.json(data)) // sending back confirmation message to client/frontend that category has been added
 		.catch((error) => {
 			console.error("Error adding category:", error); // adding error message to console
