07d001d8b78d7fc4c25cbc67a9cf435194226228
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-05 15:19:57 +0200

    Notification is at bottom on small screens
    - Bottom position could be better since at top it can prevent clicking navigation drawer open

--- client/src/components/notification/NotificationSnackbar.tsx
+++ client/src/components/notification/NotificationSnackbar.tsx
@@ -1,11 +1,20 @@
 import { useDispatch } from 'react-redux';
-import { Snackbar, Alert, Badge, Box } from '@mui/material';
+import {
+  Snackbar,
+  Alert,
+  Badge,
+  Box,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
 import { useAppSelector } from '../../app/hooks';
 import { removeNotification } from '../../features/notificationSlice';
 
 export const NotificationSnackbar = () => {
   const notifications = useAppSelector((state) => state.notification.queue);
   const dispatch = useDispatch();
+  const theme = useTheme();
+  const isSmallOrUnder = useMediaQuery(theme.breakpoints.down('sm'));
 
   const open = notifications.length > 0;
   const currentNotification =
@@ -31,8 +40,11 @@ export const NotificationSnackbar = () => {
       key={crypto.randomUUID()}
       open={open}
       onClose={handleClose}
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      sx={{ mt: -2 }}
+      anchorOrigin={{
+        vertical: isSmallOrUnder ? 'bottom' : 'top',
+        horizontal: 'center'
+      }}
+      sx={{ mt: isSmallOrUnder ? 0 : -2 }}
       message={currentNotification.message}
     >
       <Box sx={{ position: 'relative' }}>
