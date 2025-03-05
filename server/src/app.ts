2beaf5a66eb6b4bdba332fb28166170e8cb4c2af
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-03-05 13:33:54 +0200

    changes
    Co-authored-by: Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import pingRouter from './routers/pingRouter';
+import express from "express";
+import cors from "cors";
+import pingRouter from "./routers/pingRouter";
 import inventoryRouter from "./routers/inventoryRouter";
 import categoryRouter from "./routers/categoryRouter";
-import { requestLogger } from './utils/middleware';
-import path from 'path'
+import { requestLogger } from "./utils/middleware";
+import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-app.use('/api/ping', pingRouter);
+app.use("/api/ping", pingRouter);
 app.use("/api/reports/inventory", inventoryRouter);
-app.use("/api/category", categoryRouter); //router for adding category
+app.use("/api/manage/categories", categoryRouter);
+app.use("/api/manage/items", itemRouter);
 
 /** 
 app.get('/api/reports/inventory', (req, res) => {
@@ -29,8 +30,8 @@ app.get('/api/reports/inventory', (req, res) => {
 });
 */
 
-app.get('/*', (req, res) => {
-  res.status(404).json({ message: 'Unknown endpoint!' });
+app.get("/*", (req, res) => {
+	res.status(404).json({ message: "Unknown endpoint!" });
 });
 
 export default app;
