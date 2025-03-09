516efbe596c4a4321d6075c7345ec46e1254b99c
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-09 11:49:10 +0200

    Creating database connection before server startup
    Co-Authored-By: Jasse Merivirta <71035876+jayzstep@users.noreply.github.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
 import logger from './utils/logger';
 import app from './app';
+import { connectToDatabase } from './database/database';
+import { port } from './config';
 
-const port = process.env.PORT || 3000;
+const startServer = async () => {
+  logger.info(`Starting server with node env ${process.env.NODE_ENV}.`);
 
-app.listen(port, () => {
-  logger.info(
-    `Server is running on port ${port} with node env ${process.env.NODE_ENV}`
-  );
-});
+  const { isConnectionSuccessful } = await connectToDatabase();
 
-export default app;
+  if (!isConnectionSuccessful) {
+    logger.info(`Exiting server process because database connection failed.`);
+
+    // NOTE: Leaves the terminal "hanging"? Is this an issue?
+    process.exit(1);
+  }
+
+  app.listen(port, () => {
+    logger.info(`Server is running on port ${port}.`);
+  });
+};
+
+startServer();
