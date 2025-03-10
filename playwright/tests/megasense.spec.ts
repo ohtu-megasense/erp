5e227c73163fbf4770957df7f62d10fbf7ba8c7e
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-03-10 12:18:46 +0200

    fixed playwrigth test address

--- playwright/tests/megasense.spec.ts
+++ playwright/tests/megasense.spec.ts
@@ -1,9 +1,12 @@
 import { test, expect } from "@playwright/test";
+import { baseUrl } from "../config";
 
-const url = "http://localhost:5173";
+const url = baseUrl;
+test.beforeEach(async ({ page }) => {
+	await page.goto(url);
+});
 
 test("contains megasense text", async ({ page }) => {
-	await page.goto(url);
-
+	page.setDefaultTimeout(0);
 	await expect(page.getByText("Megasense")).toBeVisible();
 });
