a51ad1b26553037c0716d9238f314bb2729ed2ba
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-12 11:15:25 +0200

    Removed unused search page from App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from './components/theme-provider/ThemeProvider';
 import { DashboardPage } from './components/pages/dashboard/DashboardPage';
-import { DetailViewPage } from './components/pages/detail-view/DetailViewPage';
 import { Route, Routes } from 'react-router';
 import { DefaultPageLayout } from './components/pages/layout/DefaultPageLayout';
 import { Redirect } from './components/redirect/Redirect';
@@ -22,7 +21,6 @@ export const App = () => {
             element={<VisualizeCategoriesPage />}
           />
           <Route path="/categories/manage" element={<ManageCategoriesPage />} />
-          <Route path="/search" element={<DetailViewPage />} />
         </Route>
         <Route path="*" element={<Redirect to="/" />} />
       </Routes>
