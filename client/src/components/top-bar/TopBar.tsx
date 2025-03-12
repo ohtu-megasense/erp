dc6698c9fbcd50c87b4a0a5ee54d138baee4e116
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-12 11:21:18 +0200

    Removed ui mockup text from top bar
    - Alot of changes because of prettier replacing " to '

--- client/src/components/top-bar/TopBar.tsx
+++ client/src/components/top-bar/TopBar.tsx
@@ -2,16 +2,15 @@ import {
   AppBar,
   Divider,
   Toolbar,
-  Typography,
   useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { OpenDrawerButton } from "../drawer/OpenDrawerButton";
-import { CompanyLinkSmall } from "../company/CompanyLinkSmall";
+  useTheme
+} from '@mui/material';
+import { OpenDrawerButton } from '../drawer/OpenDrawerButton';
+import { CompanyLinkSmall } from '../company/CompanyLinkSmall';
 
 export const TopBar = () => {
   const theme = useTheme();
-  const isAtleastLarge = useMediaQuery(theme.breakpoints.up("lg"));
+  const isAtleastLarge = useMediaQuery(theme.breakpoints.up('lg'));
 
   return (
     <>
@@ -19,12 +18,12 @@ export const TopBar = () => {
         position="fixed"
         sx={{
           width: isAtleastLarge
-            ? "calc(100% - var(--mui-palette-vars-mui-drawer-width))"
+            ? 'calc(100% - var(--mui-palette-vars-mui-drawer-width))'
             : undefined,
-          bgcolor: "background.default",
+          bgcolor: 'background.default'
         }}
       >
-        <Toolbar sx={{ gap: 2, color: "text.primary" }}>
+        <Toolbar sx={{ gap: 2, color: 'text.primary' }}>
           {!isAtleastLarge && (
             <>
               <OpenDrawerButton />
@@ -33,12 +32,9 @@ export const TopBar = () => {
                 orientation="vertical"
                 sx={{
                   height: 24,
-                  bgcolor: "primary.light",
+                  bgcolor: 'primary.light'
                 }}
               />
-              <Typography variant="caption" component="span">
-                UI Mockup
-              </Typography>
             </>
           )}
         </Toolbar>
