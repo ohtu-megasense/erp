180e607b7651bc8191c101757767bee2842b6a14
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-17 20:30:19 +0200

    Debugging

--- playwright/tests/manage-categories.spec.ts
+++ playwright/tests/manage-categories.spec.ts
@@ -1,36 +1,40 @@
 import { test, expect } from "@playwright/test";
 import { baseUrl } from "../config";
 
+// Locally both work when running docker
 const pageUrl = baseUrl + "/categories/manage";
 const apiBaseUrl = "http://localhost:3000";
 
-test.beforeEach(async ({ request }) => {
-  const response = await request.post(apiBaseUrl + "/api/testing/reset");
-  expect(response.status()).toBe(200);
-});
-
-// test("Dummy test", async ({ page }) => {
-//   await page.goto(pageUrl);
-//   await expect(page.getByText("Megasense")).toBeVisible();
+// test.beforeEach(async ({ request }) => {
+//   const response = await request.post(apiBaseUrl + "/api/testing/reset");
+//   expect(response.status()).toBe(200);
 // });
 
-test("category with 2 properties can be created", async ({ page }) => {
+test("Dummy test", async ({ page, request }) => {
   await page.goto(pageUrl);
+  await expect(page.getByText("Megasense")).toBeVisible();
 
-  await page.locator("input").first().fill("New category");
-
-  await page.click("text=Add property");
-  await page.locator("input").nth(1).fill("Property1");
-
-  await page.click("text=Add property");
-  await page.locator("input").nth(2).fill("Property2");
-
-  await expect(page.locator("input")).toHaveCount(3);
-
-  await page.click("button:has-text('Create')");
-
-  await expect(page.getByText("New category created")).toBeVisible();
+  const response = await request.get(baseUrl + "/api/manage/categories");
+  console.log("Response from api categories " + response.body());
 });
+
+// test("category with 2 properties can be created", async ({ page }) => {
+//   await page.goto(pageUrl);
+
+//   await page.locator("input").first().fill("New category");
+
+//   await page.click("text=Add property");
+//   await page.locator("input").nth(1).fill("Property1");
+
+//   await page.click("text=Add property");
+//   await page.locator("input").nth(2).fill("Property2");
+
+//   await expect(page.locator("input")).toHaveCount(3);
+
+//   await page.click("button:has-text('Create')");
+
+//   await expect(page.getByText("New category created")).toBeVisible();
+// });
 
 // test("items can be added to a category", async ({ page }) => {
 //   await page.goto(pageUrl);
