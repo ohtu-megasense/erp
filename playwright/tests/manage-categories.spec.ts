ad47b909fd19baf1a75a87757f2053a6e0b536d3
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-17 21:15:02 +0200

    Uncommented another test

--- playwright/tests/manage-categories.spec.ts
+++ playwright/tests/manage-categories.spec.ts
@@ -36,38 +36,38 @@ test("category with 2 properties can be created", async ({ page }) => {
   await expect(page.getByText("New category created")).toBeVisible();
 });
 
-// test("items can be added to a category", async ({ page }) => {
-//   await page.goto(pageUrl);
+test("items can be added to a category", async ({ page }) => {
+  await page.goto(pageUrl);
 
-//   await page.getByTestId("category-name-input").fill("Test Category 1234");
+  await page.getByTestId("category-name-input").fill("Test Category 1234");
 
-//   await page.getByTestId("add-property-button").click();
-//   await page.getByTestId("add-property-input-1").click();
-//   await page.getByTestId("add-property-input-1").fill("Test property 1");
+  await page.getByTestId("add-property-button").click();
+  await page.getByTestId("add-property-input-1").click();
+  await page.getByTestId("add-property-input-1").fill("Test property 1");
 
-//   await page.getByTestId("add-property-button").click();
-//   await page.getByTestId("add-property-input-2").click();
-//   await page.getByTestId("add-property-input-2").fill("Test property 2");
+  await page.getByTestId("add-property-button").click();
+  await page.getByTestId("add-property-input-2").click();
+  await page.getByTestId("add-property-input-2").fill("Test property 2");
 
-//   await page.getByTestId("create-category-button").click();
+  await page.getByTestId("create-category-button").click();
 
-//   await page.getByTestId("add-item-button").click();
+  await page.getByTestId("add-item-button").click();
 
-//   await page
-//     .getByRole("textbox", { name: "Test property 1" })
-//     .fill("Test property 1 value");
+  await page
+    .getByRole("textbox", { name: "Test property 1" })
+    .fill("Test property 1 value");
 
-//   await page
-//     .getByRole("textbox", { name: "Test property 2" })
-//     .fill("Test property 2 value");
+  await page
+    .getByRole("textbox", { name: "Test property 2" })
+    .fill("Test property 2 value");
 
-//   await page.getByRole("button", { name: "+ Add" }).click();
+  await page.getByRole("button", { name: "+ Add" }).click();
 
-//   await expect(
-//     page.getByRole("cell", { name: "Test property 1 value" })
-//   ).toBeVisible();
+  await expect(
+    page.getByRole("cell", { name: "Test property 1 value" })
+  ).toBeVisible();
 
-//   await expect(
-//     page.getByRole("cell", { name: "Test property 2 value" })
-//   ).toBeVisible();
-// });
+  await expect(
+    page.getByRole("cell", { name: "Test property 2 value" })
+  ).toBeVisible();
+});
