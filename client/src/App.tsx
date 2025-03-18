d9042191b3fa1e6dace6964172a8d657725fd028
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-18 08:45:30 +0200

    Removed debug things

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,8 @@ import { Redirect } from './components/redirect/Redirect';
 import { VisualizeCategoriesPage } from './components/pages/categories/visualize/VisualizeCategoriesPage';
 import { ManageCategoriesPage } from './components/pages/categories/manage/ManageCategoriesPage';
 import { NotificationSnackbar } from './components/notification/NotificationSnackbar';
-import { useGetPingQuery } from './features/apiSlice';
 
 export const App = () => {
-  // const { data, error } = useGetCategoriesQuery();
-  const { data, error } = useGetPingQuery();
-
-  console.log('App test ping query', data, 'error', error);
-
   return (
     <ThemeProvider>
       <CssBaseline />
