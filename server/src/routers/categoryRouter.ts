6546ec90700ca8b1bda5a8b86a935555b1b0f207
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-19 09:54:11 +0200

    Refactored add category endpoint

--- server/src/routers/categoryRouter.ts
+++ server/src/routers/categoryRouter.ts
@@ -1,56 +1,56 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
-	AddCategory,
-	GetCategories,
-	AlterCategory,
-} from "../database/database_handler";
+  GetCategories,
+  AlterCategory,
+  addCategory
+} from '../database/database_handler';
+import { toAddCategoryRequest } from '../utils/parsers';
 
 const router = Router();
 
-router.post("/", (req, res) => {
-	const { name, itemShape } = req.body; // extracting name from request body as string
-	console.log("routerissa:", name, itemShape);
+router.post('/', async (req, res) => {
+  try {
+    const addCategoryRequest = toAddCategoryRequest(req.body);
+    const newCategory = addCategory(addCategoryRequest);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
 
-	if (!name || Object.keys(itemShape).length === 0) {
-		console.error("Category needs name or item needs shape");
-		res
-			.status(500)
-			.json({ error: "Category needs a name or item needs shape" });
-		return;
-	}
+      if (error.name === 'PARSING_ERROR') {
+        res.status(404).json({ error: error.message });
+        return;
+      }
+    }
 
-	AddCategory(name, itemShape)
-		.then((data) => res.json(data)) // sending back confirmation message to client/frontend that category has been added
-		.catch((error) => {
-			console.error("Error adding category:", error); // adding error message to console
-			res.status(500).json({ error: "Internal server error" }); // adding error message so that client/frontend can display it
-		});
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
-router.get("/", (req, res) => {
-	GetCategories()
-		.then((data) => res.json(data))
-		.catch((error) => console.error("Error getting data: ", error));
+router.get('/', (req, res) => {
+  GetCategories()
+    .then((data) => res.json(data))
+    .catch((error) => console.error('Error getting data: ', error));
 });
 
-router.put("/:categoryId", (req, res) => {
-	const { categoryId } = req.params;
-	const { itemShape } = req.body;
+router.put('/:categoryId', (req, res) => {
+  const { categoryId } = req.params;
+  const { itemShape } = req.body;
 
-	if (!categoryId || !itemShape) {
-		console.error("Category ID and new item shape are required");
-		res.status(400).json({ error: "Category ID and item shape are required" });
-		return;
-	}
+  if (!categoryId || !itemShape) {
+    console.error('Category ID and new item shape are required');
+    res.status(400).json({ error: 'Category ID and item shape are required' });
+    return;
+  }
 
-	AlterCategory(categoryId, itemShape)
-		.then(() =>
-			res.json({ message: `Category ${categoryId} updated successfully` }),
-		)
-		.catch((error) => {
-			console.error("Error updating category:", error);
-			res.status(500).json({ error: "Internal server error" });
-		});
+  AlterCategory(categoryId, itemShape)
+    .then(() =>
+      res.json({ message: `Category ${categoryId} updated successfully` })
+    )
+    .catch((error) => {
+      console.error('Error updating category:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
 });
 
 export default router;
