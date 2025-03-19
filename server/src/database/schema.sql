4e4442646709fb00d3344414d756c9d7b74c05dc
Jasse <jasse.merivirta@helsinki.fi>
2025-03-19 10:57:22 +0200

    fix hot reloading for backend

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,14 +1,28 @@
-DROP TABLE IF EXISTS item;
-DROP TABLE IF EXISTS category;
-
-CREATE TABLE IF NOT EXISTS category (
+CREATE TABLE items(
     id SERIAL PRIMARY KEY,
-    category_name TEXT NOT NULL,
-    item_shape JSONB
+    item_name TEXT
 );
 
-CREATE TABLE IF NOT EXISTS item (
-    id SERIAL PRIMARY KEY,
-    category_id INT REFERENCES category (id),
-    item_data JSONB
+CREATE TABLE value_types(
+    value_id SERIAL PRIMARY KEY,
+    value_name TEXT,
+    value_type TEXT
 );
+
+CREATE TABLE values(
+    item_id INT REFERENCES items(id),
+    value_id INT REFERENCES value_types(value_id)
+);
+
+
+CREATE TABLE app_metrics (
+    id SERIAL PRIMARY KEY,
+    app_name VARCHAR(50),
+    platform VARCHAR(10), -- 'iOS' or 'Android'
+    downloads INT DEFAULT 0,
+    active_subscriptions INT DEFAULT 0,
+    revenue DECIMAL(10,2),
+    last_updated TIMESTAMP DEFAULT NOW()
+);
+
+
