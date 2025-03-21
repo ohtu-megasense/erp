b8d04b2c0f5e8934e6d1e9feb881b08317652611
Elias Tuhola <elias.tuhola@gmail.com>
2025-03-21 10:31:46 +0200

    Added new type GetCategoriesResponse

--- shared/types.ts
+++ shared/types.ts
@@ -1,7 +1,3 @@
-// Not yet in use. Added as an example.
-// Would be used on client in apiSlice
-// and on server when parsing the request.
-
 export interface AddCategoryRequest {
   name: string;
   itemShape: Record<string, string>;
@@ -21,6 +17,8 @@ export interface AddItemResponse {
 export interface PingResponse {
   message: string;
 }
+
+export type GetCategoriesResponse = Category[];
 
 export interface Category {
   id: number;
