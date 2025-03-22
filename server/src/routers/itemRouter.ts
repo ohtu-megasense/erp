3d4f79b6da73faa64be4e569be8cd77816e7b0a2
Jasse <jasse.merivirta@helsinki.fi>
2025-03-22 11:42:31 +0200

    refactor itemRouter to use async-await and logger and remove CheckItemIdFound

--- server/src/routers/itemRouter.ts
+++ server/src/routers/itemRouter.ts
@@ -1,42 +1,38 @@
 import { Router } from "express";
-import { AddItem, DeleteItem, CheckItemIdFound } from "../database/database_handler";
+import { AddItem, DeleteItem } from "../database/database_handler";
+import logger from "../utils/logger"
 
 const router = Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 	const { id, data } = req.body;
-	console.log(id, data);
 
 	if (!id || !data) {
-		console.error("invalid request, missing name or data");
-		res.status(500).json({ error: "category id or item data missing!" });
+		logger.error("Item creation failed: missing category ID or item data");
+		res.status(400).json({ error: "Category ID and item data are required" });
 		return;
 	}
-
-	AddItem(id, data)
-		.then((data) => res.json(data))
-		.catch((error) => {
-			console.error("Error adding item:", error);
-			res.status(500).json({ error: "Internal server error" });
-		});
+  
+  try {
+    const result = await AddItem(id, data);
+    res.json(result)
+  } catch (error) {
+    logger.error("Error adding item:", error);
+    res.status(500).json({ error: "Internal server error"})
+  }
 });
 
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
 	const id = req.params.id;
 
-	if (!CheckItemIdFound(id)) {
-		console.error("did not find the item by ID");
-		res.status(400).json({ error: "did not find the item by ID" });
-		return;
-	}
-
-	DeleteItem(id)
-	.then((data) => res.json(data))
-	.catch((error) => {
-		console.error("Error deleting item:", error);
-		res.status(500).json({ error: "Internal server error" });
-	});
+  try {
+    await DeleteItem(id)
+    res.json({ message: `Item with ID ${id} deleted successfully`})
+  } catch (error) {
+    logger.error(`Error deleting item with ID ${id}:`, error)
+    res.status(500).json({ error: "Internal server error"})
+  }
 });
 
 export default router;
