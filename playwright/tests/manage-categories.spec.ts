fcaa0476aa504629556e64e3123d34229244e16a
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-03-26 12:45:09 +0200

    fixed tests, this task should be done now

--- playwright/tests/manage-categories.spec.ts
+++ playwright/tests/manage-categories.spec.ts
@@ -64,93 +64,83 @@ test("items can be added to a category", async ({ page }) => {
 	).toBeVisible();
 });
 
-test("items are shown as a part of a category they belong to", async ({
-	page,
-}) => {
+test("test", async ({ page }) => {
 	await page.goto("/categories/manage");
-	//first category
 	await page.getByTestId("category-name-input").click();
-	await page.getByTestId("category-name-input").fill("T 1");
-	await page.getByTestId("category-name-input").press("Tab");
+	await page.getByTestId("category-name-input").fill("Example 1");
 	await page.getByTestId("add-property-button").click();
-	await page.getByTestId("add-property-button").press("Tab");
-	await page.getByTestId("add-property-input-1").fill("E 1");
 	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-input-1").click();
+	await page.getByTestId("add-property-input-1").fill("Ex 1");
 	await page.getByTestId("add-property-input-2").click();
-	await page.getByTestId("add-property-input-2").fill("E 2");
+	await page.getByTestId("add-property-input-2").fill("Ex 2");
 	await page.getByTestId("create-category-button").click();
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^Example 1No items added$/ })
+			.nth(2),
+	).toBeVisible();
 	await page
 		.locator("div")
 		.filter({ hasText: /^Add New Category$/ })
 		.getByRole("button")
 		.click();
 	await page.getByTestId("category-name-input").click();
-	await page.getByTestId("category-name-input").fill("T 2");
-	await page
-		.getByText(
-			'Category NameCategory NameDefine Item ShapeExample: "Hardware Sensor" could',
-		)
-		.click();
-	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("category-name-input").fill("Example 2");
 	await page.getByTestId("add-property-button").click();
 	await page.getByTestId("add-property-input-1").click();
-	await page.getByTestId("add-property-input-1").fill("E3");
-	await page.getByTestId("add-property-input-1").press("Tab");
-	await page.locator("form").getByRole("button").nth(1).press("Tab");
-	await page.getByTestId("add-property-input-2").fill("E4");
+	await page.getByTestId("add-property-input-1").fill("Exampleprop 1");
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-input-2").click();
+	await page.getByTestId("add-property-input-2").fill("Exampleprop 2");
 	await page.getByTestId("create-category-button").click();
-	await page.getByTestId("add-item-button").nth(1).click();
-	await page.getByRole("textbox", { name: "E 1" }).click();
-	await page.getByRole("textbox", { name: "E 1" }).fill("Ex1");
-	await page.getByRole("textbox", { name: "E 1" }).press("Tab");
-	await page.getByRole("textbox", { name: "E 2" }).fill("Ex2");
-	await page.getByRole("button", { name: "+ Add" }).click();
-	await page.getByRole("textbox", { name: "E 1" }).click();
-	await page.getByRole("textbox", { name: "E 1" }).fill("E3");
-	await page.getByRole("textbox", { name: "E 1" }).press("Tab");
-	await page.getByRole("textbox", { name: "E 2" }).fill("E4");
-	await page.getByRole("button", { name: "+ Add" }).click();
-	await page.getByRole("button", { name: "Hide add form" }).click();
-	await page.getByTestId("add-item-button").first().click();
-	await page.getByRole("textbox", { name: "E3" }).click();
-	await page.getByRole("textbox", { name: "E3" }).fill("S1");
-	await page.getByRole("textbox", { name: "E3" }).press("Tab");
-	await page.getByRole("textbox", { name: "E4" }).fill("S2");
-	await page.getByRole("button", { name: "+ Add" }).click();
-	await page.getByRole("textbox", { name: "E3" }).click();
-	await page.getByRole("textbox", { name: "E3" }).fill("S3");
-	await page.getByRole("textbox", { name: "E4" }).click();
-	await page.getByRole("textbox", { name: "E4" }).fill("S4");
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^Example 2No items added$/ })
+			.first(),
+	).toBeVisible();
+	await page
+		.locator("div")
+		.filter({ hasText: /^Example 1No items added$/ })
+		.getByTestId("add-item-button")
+		.click();
+	await page.getByRole("textbox", { name: "Ex 1" }).click();
+	await page.getByRole("textbox", { name: "Ex 1" }).fill("example1");
+	await page.getByRole("textbox", { name: "Ex 2" }).click();
+	await page.getByRole("textbox", { name: "Ex 2" }).fill("example2");
 	await page.getByRole("button", { name: "+ Add" }).click();
 	await page.getByRole("button", { name: "Hide add form" }).click();
-	await expect(page.getByRole("columnheader", { name: "E3" })).toBeVisible();
-	await expect(page.getByRole("columnheader", { name: "E4" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "S1" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "S2" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "S3" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "S4" })).toBeVisible();
-	await expect(page.getByRole("columnheader", { name: "E 1" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "Ex1" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "E3" })).toBeVisible();
-	await expect(page.getByRole("columnheader", { name: "E 2" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "Ex2" })).toBeVisible();
-	await expect(page.getByRole("cell", { name: "E4" })).toBeVisible();
 	await expect(
-		page.getByRole("columnheader", { name: "ID" }).nth(1),
+		page
+			.locator("div")
+			.filter({ hasText: /^Example 1IDEx 1Ex 2[0-9]+example1example2$/ })
+			.first(),
 	).toBeVisible();
+	await page
+		.locator("div")
+		.filter({ hasText: /^Example 2No items added$/ })
+		.getByTestId("add-item-button")
+		.click();
+	await page.getByRole("textbox", { name: "Exampleprop 1" }).click();
+	await page.getByRole("textbox", { name: "Exampleprop 1" }).fill("prop1");
+	await page.getByRole("textbox", { name: "Exampleprop 2" }).click();
+	await page.getByRole("textbox", { name: "Exampleprop 2" }).fill("prop2");
+	await page.getByRole("button", { name: "+ Add" }).click();
+	await page.getByRole("textbox", { name: "Exampleprop 1" }).click();
+	await page.getByRole("textbox", { name: "Exampleprop 1" }).fill("prop3");
+	await page.getByRole("textbox", { name: "Exampleprop 2" }).click();
+	await page.getByRole("textbox", { name: "Exampleprop 2" }).fill("prop4");
+	await page.getByRole("button", { name: "+ Add" }).click();
+	await page.getByRole("button", { name: "Hide add form" }).click();
 	await expect(
-		page.getByRole("columnheader", { name: "ID" }).first(),
-	).toBeVisible();
-	await expect(
-		page.getByRole("cell", { name: "3", exact: true }),
-	).toBeVisible();
-	await expect(
-		page.getByRole("cell", { name: "4", exact: true }),
-	).toBeVisible();
-	await expect(
-		page.getByRole("cell", { name: "1", exact: true }),
-	).toBeVisible();
-	await expect(
-		page.getByRole("cell", { name: "2", exact: true }),
+		page
+			.locator("div")
+			.filter({
+				hasText:
+					/^Example 2IDExampleprop 1Exampleprop 2[0-9]+prop1prop2[0-9]+prop3prop4$/,
+			})
+			.first(),
 	).toBeVisible();
 });
