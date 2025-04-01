b2bd2c2575c294454acecfbbee3eb7fb748caf50
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-04-01 15:10:26 +0300

    Modified headers in CRM and Inventory modules for showing specific information

--- client/src/components/pages/categories/CRM/HeadingSection.tsx
+++ client/src/components/pages/categories/CRM/HeadingSection.tsx
@@ -1,12 +1,12 @@
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
 export const HeadingSection = () => {
-  return (
-    <>
-      <Typography variant="h4">Manage CRM</Typography>
-      <Typography variant="caption" ml={0.5}>
-        Actions to manage data in categories
-      </Typography>
-    </>
-  );
+	return (
+		<>
+			<Typography variant="h4">Manage CRM</Typography>
+			<Typography variant="caption" ml={0.5}>
+				Actions to manage data in CRM
+			</Typography>
+		</>
+	);
 };
--- client/src/components/pages/categories/manage/HeadingSection.tsx
+++ client/src/components/pages/categories/manage/HeadingSection.tsx
@@ -1,12 +1,12 @@
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
 export const HeadingSection = () => {
-  return (
-    <>
-      <Typography variant="h4">Manage Data</Typography>
-      <Typography variant="caption" ml={0.5}>
-        Actions to manage data in categories
-      </Typography>
-    </>
-  );
+	return (
+		<>
+			<Typography variant="h4">Manage Inventory</Typography>
+			<Typography variant="caption" ml={0.5}>
+				Actions to manage data in Inventory
+			</Typography>
+		</>
+	);
 };
