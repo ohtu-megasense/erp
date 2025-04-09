e6c270a6082199fcf528ef887c2012ac4017f183
Joonas Tapio <joonas.tapio@helsinki.fi>
2025-04-09 11:53:12 +0300

    add playwright tests and repair what is broken

--- playwright/tests/manage-categories.spec.ts
+++ playwright/tests/manage-categories.spec.ts
@@ -13,72 +13,90 @@ test("dummy test", async ({ page }) => {
 test("category with 2 properties can be created", async ({ page }) => {
 	await page.goto("/categories/inventory");
 
-	await page.locator("input").first().fill("New category");
-
-	await page.click("text=Add property");
-	await page.locator("input").nth(1).fill("Property1");
-
-	await page.click("text=Add property");
-	await page.locator("input").nth(2).fill("Property2");
-
-	await expect(page.locator("input")).toHaveCount(3);
-
-	await page.click("button:has-text('Create')");
-
+	await page.getByTestId("category-name-input").click();
+	await page.getByTestId("category-name-input").fill("Test category");
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-input-1").click();
+	await page.getByTestId("add-property-input-1").fill("Property 1");
+	await page.locator("#propertyType-0").click();
+	await page.getByRole("option", { name: "TEXT" }).click();
+	await page.getByTestId("add-property-input-2").click();
+	await page.getByTestId("add-property-input-2").fill("Property 2");
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "INTEGER" }).click();
+	await page.getByTestId("create-category-button").click();
 	await expect(page.getByText("New category created")).toBeVisible();
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^Test categoryNo items added$/ })
+			.nth(2),
+	).toBeVisible();
 });
 
 test("items can be added to a category", async ({ page }) => {
 	await page.goto("/categories/inventory");
 
+	await page.getByTestId("category-name-input").click();
 	await page.getByTestId("category-name-input").fill("Test Category 1234");
-
+	await page.getByTestId("add-property-button").click();
 	await page.getByTestId("add-property-button").click();
 	await page.getByTestId("add-property-input-1").click();
 	await page.getByTestId("add-property-input-1").fill("Test property 1");
-
-	await page.getByTestId("add-property-button").click();
+	await page.locator("#propertyType-0").click();
+	await page.getByRole("option", { name: "TEXT" }).click();
 	await page.getByTestId("add-property-input-2").click();
 	await page.getByTestId("add-property-input-2").fill("Test property 2");
-
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "INTEGER" }).click();
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-input-3").click();
+	await page.getByTestId("add-property-input-3").fill("Test property 3");
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "FLOAT" }).click();
 	await page.getByTestId("create-category-button").click();
-
 	await page.getByTestId("add-item-button").click();
-
+	await page.getByRole("textbox", { name: "Test property 1 TEXT" }).click();
 	await page
-		.getByRole("textbox", { name: "Test property 1" })
-		.fill("Test property 1 value");
-
+		.getByRole("textbox", { name: "Test property 1 TEXT" })
+		.fill("Test value 1");
+	await page.getByRole("textbox", { name: "Test property 2 INTEGER" }).click();
 	await page
-		.getByRole("textbox", { name: "Test property 2" })
-		.fill("Test property 2 value");
-
+		.getByRole("textbox", { name: "Test property 2 INTEGER" })
+		.fill("123");
+	await page.getByRole("textbox", { name: "Test property 3 FLOAT" }).click();
+	await page
+		.getByRole("textbox", { name: "Test property 3 FLOAT" })
+		.fill("123.123");
 	await page.getByRole("button", { name: "+ Add" }).click();
-
 	await expect(
-		page.getByRole("cell", { name: "Test property 1 value" }),
-	).toBeVisible();
-
-	await expect(
-		page.getByRole("cell", { name: "Test property 2 value" }),
+		page
+			.locator("div")
+			.filter({
+				hasText:
+					/^IDTest property 1Test property 2Test property 3[0-9]+Test value 1123123\.123$/,
+			})
+			.first(),
 	).toBeVisible();
 });
 
-test("test", async ({ page }) => {
+test("multiple categories can be created and items can be deleted", async ({
+	page,
+}) => {
 	await page.goto("/categories/inventory");
 	await page.getByTestId("category-name-input").click();
-	await page.getByTestId("category-name-input").fill("Example 1");
+	await page.getByTestId("category-name-input").fill("Ex1");
 	await page.getByTestId("add-property-button").click();
-	await page.getByTestId("add-property-button").click();
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "TEXT" }).click();
 	await page.getByTestId("add-property-input-1").click();
-	await page.getByTestId("add-property-input-1").fill("Ex 1");
-	await page.getByTestId("add-property-input-2").click();
-	await page.getByTestId("add-property-input-2").fill("Ex 2");
+	await page.getByTestId("add-property-input-1").fill("test1");
 	await page.getByTestId("create-category-button").click();
 	await expect(
 		page
 			.locator("div")
-			.filter({ hasText: /^Example 1No items added$/ })
+			.filter({ hasText: /^Ex1No items added$/ })
 			.nth(2),
 	).toBeVisible();
 	await page
@@ -87,60 +105,114 @@ test("test", async ({ page }) => {
 		.getByRole("button")
 		.click();
 	await page.getByTestId("category-name-input").click();
-	await page.getByTestId("category-name-input").fill("Example 2");
+	await page.getByTestId("category-name-input").fill("Ex2");
 	await page.getByTestId("add-property-button").click();
 	await page.getByTestId("add-property-input-1").click();
-	await page.getByTestId("add-property-input-1").fill("Exampleprop 1");
-	await page.getByTestId("add-property-button").click();
-	await page.getByTestId("add-property-input-2").click();
-	await page.getByTestId("add-property-input-2").fill("Exampleprop 2");
+	await page.getByTestId("add-property-input-1").fill("example1");
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "INTEGER" }).click();
 	await page.getByTestId("create-category-button").click();
 	await expect(
 		page
 			.locator("div")
-			.filter({ hasText: /^Example 2No items added$/ })
+			.filter({ hasText: /^Ex2No items added$/ })
 			.first(),
 	).toBeVisible();
-	await page
-		.locator("div")
-		.filter({ hasText: /^Example 1No items added$/ })
-		.getByTestId("add-item-button")
-		.click();
-	await page.getByRole("textbox", { name: "Ex 1" }).click();
-	await page.getByRole("textbox", { name: "Ex 1" }).fill("example1");
-	await page.getByRole("textbox", { name: "Ex 2" }).click();
-	await page.getByRole("textbox", { name: "Ex 2" }).fill("example2");
+	await page.getByTestId("add-item-button").first().click();
+	await page.getByRole("textbox", { name: "test1 TEXT" }).click();
+	await page.getByRole("textbox", { name: "test1 TEXT" }).fill("example");
 	await page.getByRole("button", { name: "+ Add" }).click();
 	await page.getByRole("button", { name: "Hide add form" }).click();
 	await expect(
 		page
 			.locator("div")
-			.filter({ hasText: /^Example 1IDEx 1Ex 2[0-9]+example1example2$/ })
+			.filter({ hasText: /^Ex1IDtest1[0-9]+example$/ })
 			.first(),
 	).toBeVisible();
-	await page
-		.locator("div")
-		.filter({ hasText: /^Example 2No items added$/ })
-		.getByTestId("add-item-button")
-		.click();
-	await page.getByRole("textbox", { name: "Exampleprop 1" }).click();
-	await page.getByRole("textbox", { name: "Exampleprop 1" }).fill("prop1");
-	await page.getByRole("textbox", { name: "Exampleprop 2" }).click();
-	await page.getByRole("textbox", { name: "Exampleprop 2" }).fill("prop2");
-	await page.getByRole("button", { name: "+ Add" }).click();
-	await page.getByRole("textbox", { name: "Exampleprop 1" }).click();
-	await page.getByRole("textbox", { name: "Exampleprop 1" }).fill("prop3");
-	await page.getByRole("textbox", { name: "Exampleprop 2" }).click();
-	await page.getByRole("textbox", { name: "Exampleprop 2" }).fill("prop4");
+	await page.getByTestId("add-item-button").nth(1).click();
+	await page.getByRole("textbox", { name: "example1 INTEGER" }).click();
+	await page.getByRole("textbox", { name: "example1 INTEGER" }).fill("123");
 	await page.getByRole("button", { name: "+ Add" }).click();
 	await page.getByRole("button", { name: "Hide add form" }).click();
 	await expect(
 		page
 			.locator("div")
-			.filter({
-				hasText:
-					/^Example 2IDExampleprop 1Exampleprop 2[0-9]+prop1prop2[0-9]+prop3prop4$/,
-			})
+			.filter({ hasText: /^Ex2IDexample1[0-9]+123$/ })
+			.first(),
+	).toBeVisible();
+	await page.getByRole("button", { name: "Edit category" }).nth(1).click();
+	await page.getByRole("row", { name: "123" }).getByRole("button").click();
+	await expect(
+		page.getByRole("heading", { name: "Do you want to delete this" }),
+	).toBeVisible();
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^If you delete this item, it can't be undone$/ }),
+	).toBeVisible();
+	await page.getByRole("button", { name: "Yes" }).click();
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^No items added$/ })
+			.first(),
+	).toBeVisible();
+	await page.getByRole("button", { name: "Save changes" }).click();
+	await page.getByRole("button", { name: "Save" }).click();
+	await expect(page.locator("#root")).toMatchAriaSnapshot(`
+    - main:
+      - heading "Manage Inventory" [level=4]
+      - text: Actions to manage data in Inventory
+      - paragraph: Create new categories
+      - button
+      - heading "Add New Category" [level=6]
+      - paragraph: Ex1
+      - button "Edit category"
+      - button "Show add form"
+      - table:
+        - rowgroup:
+          - row "ID test1":
+            - columnheader "ID"
+            - columnheader "test1"
+        - rowgroup:
+          - row /\\d+ example/:
+            - cell /\\d+/
+            - cell "example"
+      - paragraph: Ex2
+      - button "Edit category"
+      - button "Show add form"
+      - text: No items added
+    `);
+});
+test("int and float values get added correctly", async ({ page }) => {
+	await page.goto("/categories/inventory");
+	await page.getByTestId("category-name-input").click();
+	await page.getByTestId("category-name-input").fill("Ints and floats");
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-button").click();
+	await page.getByTestId("add-property-input-1").click();
+	await page.getByTestId("add-property-input-1").fill("int");
+	await page.locator("#propertyType-0").click();
+	await page.getByRole("option", { name: "INTEGER" }).click();
+	await page.getByTestId("add-property-input-2").click();
+	await page.getByTestId("add-property-input-2").fill("float");
+	await page.getByLabel("", { exact: true }).click();
+	await page.getByRole("option", { name: "FLOAT" }).click();
+	await page.getByTestId("create-category-button").click();
+	await page.getByTestId("add-item-button").click();
+	await page.getByRole("textbox", { name: "int INTEGER" }).click();
+	await page.getByRole("textbox", { name: "int INTEGER" }).fill("d");
+	await page.getByRole("textbox", { name: "float FLOAT" }).click();
+	await page.getByRole("textbox", { name: "float FLOAT" }).fill("s");
+	await page.getByRole("textbox", { name: "int INTEGER" }).click();
+	await page.getByRole("textbox", { name: "int INTEGER" }).fill("123");
+	await page.getByRole("textbox", { name: "float FLOAT" }).click();
+	await page.getByRole("textbox", { name: "float FLOAT" }).fill("123.123");
+	await page.getByRole("button", { name: "+ Add" }).click();
+	await expect(
+		page
+			.locator("div")
+			.filter({ hasText: /^IDintfloat[0-9]+123123\.123$/ })
 			.first(),
 	).toBeVisible();
 });
