4196e7587d33f660068432228e30e53ac2b53866
Jasse <jasse.merivirta@helsinki.fi>
2025-04-11 15:53:12 +0300

    add test for AND filter

--- server/test/unit/filterTest.ts
+++ server/test/unit/filterTest.ts
@@ -2,52 +2,71 @@ import { describe, test } from 'node:test';
 import assert from 'node:assert';
 import { PropertyFilter, AndFilter } from "../../src/filters/filters"
 
-describe('Filter ', () => {
-  const items = [
-    {
-      item_data: {
-        name: 'Sensor 1',
-        customer: 'Aalto University',
-        status: 'active',
-        location: 'Helsinki'
-      }
-    },
-    {
-      item_data: {
-        name: 'Sensor 2',
-        customer: 'Aalto University',
-        status: 'active',
-        location: 'Mäntsälä'
-      }
-    },
-    {
-      item_data: {
-        name: 'Sensor 3',
-        customer: 'University of Helsinki',
-        status: 'not active',
-        location: 'Iisalmi'
-      }
-    },
-    {
-      item_data: {
-        name: '',
-        customer: 'Aalto University',
-        status: 'active',
-        location: 'Salla'
-      }
+const items = [
+  {
+    item_data: {
+      name: 'Sensor 1',
+      customer: 'Aalto University',
+      status: 'active',
+      location: 'Helsinki'
     }
-  ];
+  },
+  {
+    item_data: {
+      name: 'Sensor 2',
+      customer: 'Aalto University',
+      status: 'active',
+      location: 'Mäntsälä'
+    }
+  },
+  {
+    item_data: {
+      name: 'Sensor 3',
+      customer: 'University of Helsinki',
+      status: 'active',
+      location: 'Iisalmi'
+    }
+  },
+  {
+    item_data: {
+      name: 'Sensor 4',
+      customer: 'Aalto University',
+      status: 'not active',
+      location: 'Salla'
+    }
+  }
+];
 
-  test('for property returns correct list', () => {
+describe('Property filter ', () => {
+  test('returns correct list', () => {
     const activeFilter = new PropertyFilter('status', 'active')
     const activeSensors = activeFilter.apply(items)
     assert.strictEqual(activeSensors.length, 3)
   })
 
-  test('with no matches returns empty list', () => {
+  test('with no matching values returns empty list', () => {
     const activeFilter = new PropertyFilter('customer', 'Hesburger')
     const activeSensors = activeFilter.apply(items)
     assert.strictEqual(activeSensors.length, 0)
   })
 
+  test('with no matching property returns empty list', () => {
+    const activeFilter = new PropertyFilter('restaurant', 'Hesburger')
+    const activeSensors = activeFilter.apply(items)
+    assert.strictEqual(activeSensors.length, 0)
+  })
+
 });
+
+describe('AND filter ', () => {
+
+  test('returns correct list', () => {
+    const activeFilter = new PropertyFilter('status', 'active')
+    const customerFilter = new PropertyFilter('customer', 'Aalto University')
+
+    const activeAndCustomerFilter = new AndFilter([activeFilter, customerFilter])
+    const activeAaltoSensors = activeAndCustomerFilter.apply(items)
+
+    assert.strictEqual(activeAaltoSensors.length, 2)
+  })
+})
