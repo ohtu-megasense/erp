15d2dd533dc47fdd124b7bda9c9d72ea25bf59f2
Jasse <jasse.merivirta@helsinki.fi>
2025-04-11 20:04:35 +0300

    move itemFilterService to Services/

--- server/src/database/database_handler.ts
+++ server/src/database/database_handler.ts
@@ -125,25 +125,6 @@ export const renameCategory = async (
   }
 };
 
-export class ItemFilterService {
-  async getFilteredItems(filter?: Filter): Promise<any[]> {
-    try {
-      const query = 'SELECT * FROM items;';
-      const result = await pool.query(query);
-      let items = result.rows;
-
-      if (filter) {
-        items = filter.apply(items);
-        logger.info(`Used filter: ${filter.getDescription()}`);
-      }
-
-      return items;
-    } catch (error) {
-      logger.error('Error filtering items:', error);
-      throw error;
-    }
-  }
-}
 
 export const getCategories = async (
   moduleName: string
---
+++ server/src/services/itemFilterService.ts
@@ -0,0 +1,23 @@
+import { Filter } from '../filters/filters'
+import { pool } from '../database/database';
+import logger from '../utils/logger';
+
+export class ItemFilterService {
+  async getFilteredItems(filter?: Filter): Promise<any[]> {
+    try {
+      const query = 'SELECT * FROM items;';
+      const result = await pool.query(query);
+      let items = result.rows;
+
+      if (filter) {
+        items = filter.apply(items);
+        logger.info(`Used filter: ${filter.getDescription()}`);
+      }
+
+      return items;
+    } catch (error) {
+      logger.error('Error filtering items:', error);
+      throw error;
+    }
+  }
+}
