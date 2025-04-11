54cc610df6a2d6bd6f1970e8301605831f26c49a
Jasse <jasse.merivirta@helsinki.fi>
2025-04-11 08:57:56 +0300

    add PropertyFilter and AndFilter decorator

---
+++ server/src/filters/filters.ts
@@ -0,0 +1,37 @@
+interface Filter {
+  apply(items: any[]): any[];
+  getDescription: string;
+}
+
+class PropertyFilter implements Filter {
+  constructor(
+    private property: string,
+    private value: string
+  ) {}
+
+  apply(items: any[]): any[] {
+    return items.filter(
+      (item) =>
+        item.item_data[this.property] &&
+        item.item_data[this.property] === this.value
+    );
+  }
+
+  getDescription(): string {
+    return `${this.property} is "${this.value}"`
+  }
+}
+
+class AndFilter implements Filter {
+  constructor(
+    private filters: Filter[]
+  ) {}
+
+  apply(items: any[]): any[] {
+    return this.filters.reduce((result, filter) => filter.apply(result), items)
+  }
+
+  getDescriptio(): string {
+    return `${this.filters.map(filter => filter.getDescription()).join(' AND ')}`
+  }
+}
