613843452e6bfbc87eeedeeefa756daf821f7fc6
Jasse <jasse.merivirta@helsinki.fi>
2025-04-12 10:44:16 +0300

    add tests for filter generator
    add more tests for filter generator

---
+++ server/test/unit/filterGeneratorTest.ts
@@ -0,0 +1,59 @@
+import { describe, test } from 'node:test';
+import assert from 'node:assert';
+import { generateFilterFromConfig } from '../../src/filters/filterGenerator';
+
+const config = {
+  type: 'and',
+  filters: [
+    {
+      type: 'equals',
+      property: 'customer',
+      value: 'Aalto University'
+    },
+    {
+      type: 'equals',
+      property: 'status',
+      value: 'active'
+    }
+  ]
+};
+
+describe('Filter generator ', () => {
+  test('generates correct filter from config', () => {
+    const filter = generateFilterFromConfig(config);
+    assert.strictEqual(
+      filter.getDescription(),
+      'customer is "Aalto University" AND status is "active"'
+    );
+  });
+
+  test('returns "undefined" if no config provided', () => {
+    assert.strictEqual(generateFilterFromConfig(undefined), undefined);
+  });
+
+  test('throws an error if wrong filter type', () => {
+    const wrongConfig = {
+      type: 'notAFilter',
+      property: 'customer',
+      value: 'Puolustusvoimat'
+    };
+    assert.throws(() => generateFilterFromConfig(wrongConfig), {
+      message: 'Unknown filter type: notAFilter'
+    });
+  });
+
+  test('throws an error if AND does not get an array', () => {
+    const noArrayConfig = {
+      type: 'and',
+      filters: {
+        type: 'equals',
+        property: 'customer',
+        value: 'Aalto University'
+      }
+    }
+
+    assert.throws(() => generateFilterFromConfig(noArrayConfig), {
+      message: 'AND requires array of filters'
+    })
+  })
+});
