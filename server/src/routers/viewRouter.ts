ebf95efd00d14bcaa9274e431a66f52dbf8bb593
Jasse <jasse.merivirta@helsinki.fi>
2025-04-12 18:55:12 +0300

    add router for posting a new view

--- server/src/routers/viewRouter.ts
+++ server/src/routers/viewRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import logger from '../utils/logger';
 import { ViewsService } from '../services/viewsService';
 import { isValidModule } from '../utils/parsers';
+import { ViewConfig } from '../../../shared/types';
 
 const router = Router();
 const viewsService = new ViewsService();
@@ -17,8 +18,23 @@ router.get('/:module', async (req, res) => {
     res.json(views);
   } catch (error) {
     logger.error('Error when getting views:', error);
-    res.status(500).json({ error: 'internal server error' });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-export default router
+router.post('/', async (req, res) => {
+  try {
+    const viewConfig: ViewConfig = req.body;
+
+    if (!viewConfig.name || !viewConfig.filterConfig) {
+      res.status(400).json({ error: 'Missing required fields' });
+      return;
+    }
+    const savedView = await viewsService.saveView(viewConfig);
+    res.status(201).json(savedView);
+  } catch (error) {
+    logger.error('Error when saving view:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+export default router;
