840103f4f33caf8abbaebba3d3171a41c4dda27c
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-14 01:22:06 +0300

    Added id field to base filter and request / response types for views

--- shared/types.ts
+++ shared/types.ts
@@ -1,105 +1,112 @@
 export interface AddCategoryRequest {
-	name: string;
-	module: string;
-	itemShape: Record<string, string | number>;
+  name: string;
+  module: string;
+  itemShape: Record<string, string | number>;
 }
 
 export interface AddCategoryResponse {
-	id: number;
-	name: string;
-	itemShape: Record<string, string | number>;
+  id: number;
+  name: string;
+  itemShape: Record<string, string | number>;
 }
 
 export interface Module {
-	module: string;
+  module: string;
 }
 
 export interface renameCategoryRequest {
-	categoryId: number;
-	itemShape: Record<string, string | number>;
-	categoryName: string;
+  categoryId: number;
+  itemShape: Record<string, string | number>;
+  categoryName: string;
 }
 
 export interface renameCategoryResponse {
-	success: boolean;
-	name: string;
+  success: boolean;
+  name: string;
 }
 
 export interface DeleteCategoryRequest {
-	categoryId: number;
+  categoryId: number;
 }
 
 export interface DeleteCategoryResponse {
-	categoryName: string;
+  categoryName: string;
 }
 
 export interface AddItemResponse {
-	success: boolean;
-	data: Item;
+  success: boolean;
+  data: Item;
 }
 
 export interface PingResponse {
-	message: string;
+  message: string;
 }
 
 export type GetCategoriesResponse = Category[];
 
 export interface Category {
-	id: number;
-	name: string;
-	itemShape: Record<string, string | number>;
-	items: Item[];
+  id: number;
+  name: string;
+  itemShape: Record<string, string | number>;
+  items: Item[];
 }
 
 export interface Item {
-	id: number;
-	item_data: Record<string, string | number>;
+  id: number;
+  item_data: Record<string, string | number>;
 }
 
 export interface AddColumnRequest {
-	categoryId: number;
-	itemShape: Record<string, string | number>;
-	categoryName: string;
+  categoryId: number;
+  itemShape: Record<string, string | number>;
+  categoryName: string;
 }
 
 export interface AddColumnResponse {
-	success: boolean;
-	updatedCategory: Category;
+  success: boolean;
+  updatedCategory: Category;
 }
 
 export interface UpdateItemRequest {
-	categoryId: number;
-	itemId: number;
-	updatedItem: Record<string, string | number>;
+  categoryId: number;
+  itemId: number;
+  updatedItem: Record<string, string | number>;
 }
 
 export interface UpdateItemResponse {
-	id: number;
-	categoryId: number;
-	data: Record<string, string | number>;
+  id: number;
+  categoryId: number;
+  data: Record<string, string | number>;
 }
 
 // For Views and Filters:
 
 export interface BaseFilterConfig {
+  id: string | number;
   type: string;
 }
 
 export interface PropertyFilterConfig extends BaseFilterConfig {
-  type: 'equals';
+  type: "equals";
   property: string;
   value: string;
 }
 
 export interface AndFilterConfig extends BaseFilterConfig {
-  type: 'and';
-  filters: FilterConfig[]
+  type: "and";
+  filters: FilterConfig[];
 }
 
-export type FilterConfig = PropertyFilterConfig | AndFilterConfig
+export type FilterConfig = PropertyFilterConfig | AndFilterConfig;
 
 export interface ViewConfig {
   name: string;
   module: string;
-  filterConfig: FilterConfig
+  filterConfig: FilterConfig;
 }
+
+export type View = Omit<Category, "itemShape">;
+export type CreateViewRequest = ViewConfig;
+export type CreateViewResponse = object;
+export type GetViewsRequest = "inventory" | "crm";
+export type GetViewsResponse = View[];
