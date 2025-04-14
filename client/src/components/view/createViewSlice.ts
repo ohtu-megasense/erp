ecb2f217a57105eea5423efb46087de8b6033bef
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-14 10:20:29 +0300

    Using 'and' filter as a root for filters

--- client/src/components/view/createViewSlice.ts
+++ client/src/components/view/createViewSlice.ts
@@ -1,17 +1,25 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { FilterConfig, ViewConfig } from '../../../../shared/types';
+import {
+  AndFilterConfig,
+  FilterConfig,
+  ModuleOption
+} from '../../../../shared/types';
 
 interface CreateViewSlice {
-  filters: FilterConfig[];
-  viewConfig: ViewConfig | null;
   module: 'inventory' | 'crm';
+  name: string;
+  root: AndFilterConfig;
   propertyOptions: string[];
 }
 
 const initialState: CreateViewSlice = {
-  filters: [],
-  viewConfig: null,
   module: 'crm',
+  name: '',
+  root: {
+    id: 'root',
+    type: 'and',
+    filters: []
+  },
   propertyOptions: []
 };
 
@@ -21,24 +29,38 @@ const slice = createSlice({
   reducers: {
     removedFilter: (state, action: PayloadAction<{ id: string | number }>) => {
       const id = action.payload.id;
-      state.filters = state.filters.filter((filter) => filter.id !== id);
+      state.root.filters = state.root.filters.filter(
+        (filter) => filter.id !== id
+      );
     },
     addedFilter: (state, action: PayloadAction<{ filter: FilterConfig }>) => {
-      state.filters.push(action.payload.filter);
+      state.root.filters.push(action.payload.filter);
     },
-    createdView: (state, action: PayloadAction<{ view: ViewConfig }>) => {
-      state.viewConfig = action.payload.view;
-      state.filters = [];
-      state.viewConfig = null;
+    setName: (state, action: PayloadAction<{ name: string }>) => {
+      state.name = action.payload.name;
     },
     setPropertyOptions: (
       state,
       action: PayloadAction<{ properties: string[] }>
     ) => {
       state.propertyOptions = action.payload.properties;
+    },
+    setModule: (state, action: PayloadAction<{ module: ModuleOption }>) => {
+      state.module = action.payload.module;
+    },
+    resetState: (state) => {
+      state.name = '';
+      state.root.filters = [];
     }
   }
 });
 
 export default slice.reducer;
-export const { removedFilter, addedFilter, setPropertyOptions } = slice.actions;
+export const {
+  removedFilter,
+  addedFilter,
+  setPropertyOptions,
+  setName,
+  setModule,
+  resetState
+} = slice.actions;
