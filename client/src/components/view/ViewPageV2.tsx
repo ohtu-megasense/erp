572325ca3cc2224866ff1d4b9101590c2406dfaa
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-15 19:03:05 +0300

    View can be created from nodes
    - Some issues (using mock dataset): AND filter with Owner = Kia Fall && Manufacturing year = 2023
    - Did not work: something to do with value types?
    - But searching string types worked: AND filter with Customer = Google && Status = Shipping

--- client/src/components/view/ViewPageV2.tsx
+++ client/src/components/view/ViewPageV2.tsx
@@ -8,6 +8,11 @@ import {
   Select,
   SelectChangeEvent,
   Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
   TextField,
   Typography
 } from '@mui/material';
@@ -19,17 +24,29 @@ import {
   FilterConfig,
   FilterOption,
   filterOptions,
+  Item,
   ModuleOption,
-  moduleOptions
+  moduleOptions,
+  PropertyFilterConfig,
+  ViewConfig,
+  type View
 } from '../../../../shared/types';
-import { useEffect, useState } from 'react';
-import { useGetCategoriesQuery } from '../../features/apiSlice';
+import { ChangeEvent, useEffect, useState } from 'react';
+import {
+  useCreateViewMutation,
+  useGetCategoriesQuery,
+  useGetViewsQuery
+} from '../../features/apiSlice';
 import {
   addNode,
+  createView,
   createNode,
+  deleteNode,
   Id,
   reset,
+  saveFilter,
   setModule,
+  setName,
   setPropertyOptions,
   type Node
 } from './createViewSliceV2';
@@ -122,6 +139,7 @@ const LoadPropertyOptions = () => {
   return null;
 };
 
+// This could be "create decorator"
 const And = (props: { filter: AndFilterConfig; parentId: Id }) => {
   const { filter } = props;
 
@@ -130,11 +148,18 @@ const And = (props: { filter: AndFilterConfig; parentId: Id }) => {
 
   return (
     <Box>
-      <Stack sx={{}}>
-        <Typography>AND</Typography>
-        <AddNode parentId={filter.id} text="+ Filter" />
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 1
+        }}
+      >
+        <Typography>AND (Dekoraattorin valinta tähän)</Typography>
+        <AddButton parentId={filter.id} text="+ Filter" />
+        <DeleteButton id={props.filter.id} />
       </Stack>
-      <Stack px={2}>
+      <Stack ml={4} mt={2} gap={2}>
         {children.map((child) => (
           <Node
             key={child.filter.id}
@@ -147,17 +172,29 @@ const And = (props: { filter: AndFilterConfig; parentId: Id }) => {
   );
 };
 
-const CreateFilter = (props: { parentId: Id }) => {
+const CreateFilter = (props: {
+  filter: PropertyFilterConfig;
+  parentId: Id;
+}) => {
   const propertyOptions = useAppSelector(
     (state) => state.createView.propertyOptions
   );
 
   const [property, setProperty] = useState('');
   const [value, setValue] = useState('');
-  const [type, setType] = useState('equals'); // default
+  const [type, setType] = useState<FilterOption>('equals'); // default
 
   // on save -> send to redux and use
   // addFilter (not addNode) (not implemented)
+
+  const createFilter = (): PropertyFilterConfig => {
+    return {
+      id: props.filter.id,
+      property,
+      type,
+      value
+    };
+  };
 
   return (
     <Stack
@@ -180,26 +217,6 @@ const CreateFilter = (props: { parentId: Id }) => {
           }}
         >
           <FormControl fullWidth>
-            <InputLabel>Type</InputLabel>
-            <Select
-              value={type}
-              label="Type"
-              onChange={({ target }) => setType(target.value)}
-            >
-              {Object.values(filterOptions).map((filterOption) => (
-                <MenuItem key={filterOption} value={filterOption}>
-                  {filterOption}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Box
-          sx={{
-            minWidth: 180
-          }}
-        >
-          <FormControl fullWidth>
             <InputLabel>Property</InputLabel>
             <Select
               value={property}
@@ -214,14 +231,78 @@ const CreateFilter = (props: { parentId: Id }) => {
             </Select>
           </FormControl>
         </Box>
-        <Typography>=</Typography>
+        <Box
+          sx={{
+            minWidth: 180
+          }}
+        >
+          <FormControl fullWidth>
+            <InputLabel>Type</InputLabel>
+            <Select
+              value={type}
+              label="Type"
+              onChange={({ target }) => setType(target.value)}
+            >
+              {Object.values(filterOptions).map((filterOption) => (
+                <MenuItem key={filterOption} value={filterOption}>
+                  {filterOption}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
         <TextField
           placeholder={'Value'}
           value={value}
           onChange={({ target }) => setValue(target.value)}
         />
       </Stack>
+      <SaveButton id={props.filter.id} filter={createFilter()} />
+      <DeleteButton id={props.filter.id} />
     </Stack>
+  );
+};
+
+const SaveButton = (props: { id: Id; filter: PropertyFilterConfig }) => {
+  const { id, filter } = props;
+  const dispatch = useAppDispatch();
+
+  const onClick = () => {
+    dispatch(saveFilter({ id, filter }));
+  };
+  return (
+    <Button
+      variant="outlined"
+      size="small"
+      sx={{
+        color: greenColor,
+        borderColor: greenColor
+      }}
+      onClick={onClick}
+    >
+      +
+    </Button>
+  );
+};
+
+const DeleteButton = (props: { id: Id }) => {
+  const dispatch = useAppDispatch();
+
+  const onClick = () => {
+    dispatch(deleteNode({ id: props.id }));
+  };
+  return (
+    <Button
+      variant="outlined"
+      size="small"
+      sx={{
+        color: pinkColor,
+        borderColor: pinkColor
+      }}
+      onClick={onClick}
+    >
+      -
+    </Button>
   );
 };
 
@@ -232,7 +313,7 @@ const Node = (props: { filter: FilterConfig; parentId: Id }) => {
     case 'and':
       return <And filter={filter} parentId={parentId} />;
     case 'equals':
-      return <CreateFilter parentId={parentId} />;
+      return <CreateFilter filter={filter} parentId={parentId} />;
     default:
       throw new Error('Unknown filter config');
   }
@@ -252,20 +333,20 @@ const RootNode = () => {
         />
       ) : (
         <>
-          <AddNode parentId={-1} text="Create Filter" />
+          <AddButton parentId={-1} text="Create Filter" />
         </>
       )}
     </>
   );
 };
 
-const AddNode = (props: { parentId: Id; text?: string }) => {
+const AddButton = (props: { parentId: Id; text?: string }) => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const dispatch = useAppDispatch();
   const options = [
     ...Object.values(filterOptions),
     ...Object.values(decoratorOptions)
-  ];
+  ].sort((a, b) => a.localeCompare(b));
 
   const onClickOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -291,7 +372,9 @@ const AddNode = (props: { parentId: Id; text?: string }) => {
 
   return (
     <Box>
-      <Button onClick={onClickOpen}>{props.text || 'Create Filter'}</Button>
+      <Button onClick={onClickOpen} variant="outlined" size="small">
+        {props.text || 'Create Filter'}
+      </Button>
       <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={onClose}>
         {options.map((option) => (
           <MenuItem
@@ -315,6 +398,193 @@ const ResetButton = () => {
   };
 
   return <Button onClick={onClick}>Reset</Button>;
+};
+
+const CreateButton = () => {
+  const name = useAppSelector((state) => state.createView.name);
+  const module = useAppSelector((state) => state.createView.module);
+  const nodes = useAppSelector((state) => state.createView.nodes);
+  const [isAccepted, setIsAccepted] = useState(false);
+  const dispatch = useAppDispatch();
+  const [apiCreateView] = useCreateViewMutation();
+
+  useEffect(() => {
+    setIsAccepted(false);
+  }, [nodes]);
+
+  const getView = (): ViewConfig | null => {
+    if (!name) {
+      console.log('Invalid name');
+      return null;
+    }
+
+    if (nodes.length === 0) {
+      console.log('Add a filter first');
+      return null;
+    }
+
+    return createView(module, name, nodes);
+  };
+
+  const onClick = async () => {
+    const view = getView();
+    if (view === null) return;
+    try {
+      const response = await apiCreateView(view).unwrap();
+      dispatch(reset());
+      console.log('Created a view', response);
+    } catch (error) {
+      console.log('Error creating a view', error);
+    }
+  };
+
+  const isEnabled = nodes.length > 0 && Boolean(name) && isAccepted;
+
+  return (
+    <Stack gap={2}>
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <Typography>Tallensin Filtterit (Dekoja ei tarvi)?</Typography>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() => setIsAccepted(true)}
+          disableRipple={true}
+        >
+          Joo
+        </Button>
+      </Stack>
+      <Box>
+        <Button
+          variant="outlined"
+          disabled={!isEnabled}
+          fullWidth={false}
+          onClick={onClick}
+          sx={{
+            color: greenColor,
+            outline: '1.5px solid',
+            outlineColor: isEnabled ? greenColor : undefined,
+            borderRadius: 2,
+            px: 2
+          }}
+        >
+          Save Filter as View
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
+
+const Name = () => {
+  const name = useAppSelector((state) => state.createView.name);
+  const dispatch = useAppDispatch();
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setName({ name: event.target.value }));
+  };
+
+  return (
+    <Box>
+      <TextField
+        fullWidth={true}
+        value={name}
+        onChange={onChange}
+        placeholder="Enter filter name..."
+      />
+    </Box>
+  );
+};
+
+const ViewsList = () => {
+  const module = useAppSelector((state) => state.createView.module);
+  const { data: views = [] } = useGetViewsQuery(module);
+  const ordered = [...views].sort((a, b) => a.name.localeCompare(b.name));
+
+  return (
+    <Stack
+      sx={{
+        gap: 2
+      }}
+    >
+      <Stack>
+        <Typography
+          sx={{
+            fontSize: 18,
+            fontWeight: 500
+          }}
+        >
+          Views
+        </Typography>
+        {views.length === 0 && (
+          <Typography variant="caption">No Views Found</Typography>
+        )}
+      </Stack>
+      {ordered.map((view) => (
+        <View key={view.id} view={view} />
+      ))}
+    </Stack>
+  );
+};
+
+const View = (props: { view: View }) => {
+  const { view } = props;
+
+  const getShape = (): string[] => {
+    if (view.items.length === 0) return [];
+    return Object.keys(view.items[0].item_data);
+  };
+
+  const shape = getShape();
+
+  return (
+    <Stack
+      sx={{
+        border: '1.5px solid',
+        borderRadius: 2,
+        borderColor: blueColor,
+        py: 2
+      }}
+    >
+      <Stack px={2}>
+        <Typography>{view.name}</Typography>
+        <Typography variant="caption">{view.items.length} items</Typography>
+      </Stack>
+      {view.items.length > 0 && (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              {shape.map((property, index) => (
+                <TableCell key={index}>{property}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {view.items.map((item) => (
+              <Row key={item.id} item={item} />
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </Stack>
+  );
+};
+
+const Row = (props: { item: Item }) => {
+  const values = Object.values(props.item.item_data);
+  return (
+    <TableRow>
+      <TableCell>{props.item.id}</TableCell>
+      {values.map((value, index) => (
+        <TableCell key={index}>{value}</TableCell>
+      ))}
+    </TableRow>
+  );
 };
 
 export const ViewPageV2 = () => {
@@ -341,7 +611,16 @@ export const ViewPageV2 = () => {
           <RootNode />
         </Box>
         <Box bgcolor={debugGreyColor} p={2}>
+          <Name />
+        </Box>
+        <Box bgcolor={debugGreyColor} p={2}>
+          <CreateButton />
+        </Box>
+        <Box bgcolor={debugGreyColor} p={2}>
           <ResetButton />
+        </Box>
+        <Box bgcolor={debugGreyColor} p={2}>
+          <ViewsList />
         </Box>
       </Stack>
     </>
--- client/src/components/view/createViewSliceV2.ts
+++ client/src/components/view/createViewSliceV2.ts
@@ -3,7 +3,9 @@ import {
   DecoratorOption,
   FilterConfig,
   FilterOption,
-  ModuleOption
+  ModuleOption,
+  PropertyFilterConfig,
+  ViewConfig
 } from '../../../../shared/types';
 
 export type Id = string | number;
@@ -58,6 +60,70 @@ export const createNode = (
   }
 };
 
+export const createView = (
+  module: string,
+  name: string,
+  nodes: Node[]
+): ViewConfig | null => {
+  if (nodes.length === 0) {
+    return null;
+  }
+  const rootId = nodes[0].filter.id;
+  const filterConfig = createFilterConfig(rootId, nodes);
+
+  if (!filterConfig) {
+    console.log('Failed to build filter config from root');
+    return null;
+  }
+
+  const view: ViewConfig = {
+    module,
+    name,
+    filterConfig
+  };
+
+  return view;
+};
+
+const createFilterConfig = (rootId: Id, nodes: Node[]): FilterConfig | null => {
+  const rootNode = nodes.find((node) => node.filter.id === rootId);
+  if (!rootNode) {
+    return null;
+  }
+
+  const filter = rootNode.filter;
+
+  if (filter.type === 'equals') {
+    return {
+      id: filter.id,
+      type: 'equals',
+      property: filter.property,
+      value: filter.value
+    };
+  }
+
+  if (filter.type === 'and') {
+    const childFilters: FilterConfig[] = [];
+
+    const childNodes = nodes.filter((node) => node.parentId === rootId);
+
+    for (const childNode of childNodes) {
+      const childFilter = createFilterConfig(childNode.filter.id, nodes);
+      if (childFilter) {
+        childFilters.push(childFilter);
+      }
+    }
+
+    return {
+      id: filter.id,
+      type: 'and',
+      filters: childFilters
+    };
+  }
+
+  return null;
+};
+
 const slice = createSlice({
   name: 'createView',
   initialState,
@@ -71,9 +137,46 @@ const slice = createSlice({
     setModule: (state, action: PayloadAction<{ module: ModuleOption }>) => {
       state.module = action.payload.module;
     },
+    setName: (state, action: PayloadAction<{ name: string }>) => {
+      state.name = action.payload.name;
+    },
     addNode: (state, action: PayloadAction<{ node: Node }>) => {
       state.nodes.push(action.payload.node);
-      console.log('Added new child for parent', action.payload.node.parentId);
+    },
+    saveFilter: (
+      state,
+      action: PayloadAction<{ id: Id; filter: PropertyFilterConfig }>
+    ) => {
+      const id = action.payload.id;
+      const node = state.nodes.find((node) => node.filter.id === id);
+
+      if (!node) {
+        return;
+      }
+
+      node.filter = action.payload.filter;
+    },
+    deleteNode: (state, action: PayloadAction<{ id: Id }>) => {
+      const id = action.payload.id;
+
+      const idsToRemove = new Set<Id>();
+      idsToRemove.add(id);
+
+      const collectDescendantIds = (parentId: Id) => {
+        state.nodes.forEach((node) => {
+          if (node.parentId === parentId) {
+            idsToRemove.add(node.filter.id);
+            collectDescendantIds(node.filter.id);
+          }
+        });
+      };
+
+      collectDescendantIds(id);
+
+      state.nodes = state.nodes.filter(
+        (node) =>
+          !idsToRemove.has(node.filter.id) && !idsToRemove.has(node.parentId)
+      );
     },
     reset: (state) => {
       state.name = '';
@@ -83,4 +186,12 @@ const slice = createSlice({
 });
 
 export default slice.reducer;
-export const { setPropertyOptions, setModule, addNode, reset } = slice.actions;
+export const {
+  setPropertyOptions,
+  setModule,
+  setName,
+  addNode,
+  saveFilter,
+  deleteNode,
+  reset
+} = slice.actions;
