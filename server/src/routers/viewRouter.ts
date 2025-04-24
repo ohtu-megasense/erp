0bfe7fc2422cc33a76ff3696397e6631ba49fd6c
Jasse <jasse.merivirta@helsinki.fi>
2025-04-24 09:45:59 +0300

    add PUT to viewRouter

--- server/src/routers/viewRouter.ts
+++ server/src/routers/viewRouter.ts
@@ -47,4 +47,41 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+router.put('/:id', async (req, res) => {
+  try {
+    const viewId = parseInt(req.params.id)
+    if (isNaN(viewId)) {
+      res.status(400).json({ error: 'Filter type not supported' })
+      return
+    }
+
+    const viewConfig: ViewConfig = req.body
+    if (!supportedFilterTypes.includes(viewConfig.filterConfig.type)) {
+      res.status(400).json({ error: 'Filter type not supported' });
+      return;
+    }
+
+    const module = viewConfig.module;
+    if (!isValidModule(module)) {
+      res.status(400).json({ error: 'Invalid module parameter' });
+      return;
+    }
+    if (!viewConfig.name || !viewConfig.filterConfig) {
+      res.status(400).json({ error: 'Missing required fields' });
+      return;
+    }
+
+    const updatedView = await viewsService.updateView(viewId, viewConfig)
+    res.status(200).json(updatedView)
+
+  } catch (error) {
+    logger.error('Error when updating view:', error)
+    if (error instanceof Error && error.message.includes('not found')) {
+      res.status(404).json({ error: error.message})
+      return
+    }
+    res.status(500).json({ error: 'Internal server error'})
+  }
+})
 export default router;
