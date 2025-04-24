dfce94c6e79c4d0373f52875ccd75b3efe538202
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-24 16:20:25 +0300

    Ran prettier on main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,15 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import { BrowserRouter } from "react-router";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
