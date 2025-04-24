292290b8f7b3caf2fc138924703abe0d6bbb0f61
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-24 17:23:39 +0300

    Passing module as prop on manage data page

--- client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
+++ client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
@@ -2,12 +2,13 @@ import { Box, Stack, Typography } from '@mui/material';
 import { ManageCategoriesSection } from '../../../categories/manage/ManageCategoriesSection';
 import { AddCategorySection } from '../../../categories/manage/AddCategorySection';
 import { blueColor, orangeColor } from '../../../view/colors';
+import { ModuleOption } from '../../../../../../shared/types';
 
-const Heading = () => {
+const Heading = (props: { module: ModuleOption }) => {
   return (
     <Stack>
       <Typography sx={{ color: blueColor, fontSize: 38, fontWeight: 500 }}>
-        INVENTORY
+        {props.module.toLocaleUpperCase()}
       </Typography>
       <Typography
         sx={{
@@ -22,17 +23,17 @@ const Heading = () => {
   );
 };
 
-export const ManageCategoriesPage = () => {
+export const DataPage = (props: { module: ModuleOption }) => {
   return (
     <Box px={2}>
       <Box mt={2}>
-        <Heading />
+        <Heading module={props.module} />
       </Box>
       <Box mt={4}>
-        <AddCategorySection module="inventory" />
+        <AddCategorySection module={props.module} />
       </Box>
       <Box mt={4}>
-        <ManageCategoriesSection module="inventory" />
+        <ManageCategoriesSection module={props.module} />
       </Box>
     </Box>
   );
