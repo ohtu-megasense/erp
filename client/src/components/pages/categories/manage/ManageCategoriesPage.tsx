5af911632a9b629a4d10d29f1ec08125bd32f6f7
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-24 16:16:10 +0300

    Updated heading to be consistent with views page

--- client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
+++ client/src/components/pages/categories/manage/ManageCategoriesPage.tsx
@@ -1,20 +1,39 @@
-import { Box } from "@mui/material";
-import { HeadingSection } from "./HeadingSection";
-import { ManageCategoriesSection } from "../../../categories/manage/ManageCategoriesSection";
-import { AddCategorySection } from "../../../categories/manage/AddCategorySection";
+import { Box, Stack, Typography } from '@mui/material';
+import { ManageCategoriesSection } from '../../../categories/manage/ManageCategoriesSection';
+import { AddCategorySection } from '../../../categories/manage/AddCategorySection';
+import { blueColor, orangeColor } from '../../../view/colors';
+
+const Heading = () => {
+  return (
+    <Stack>
+      <Typography sx={{ color: blueColor, fontSize: 38, fontWeight: 500 }}>
+        INVENTORY
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: 24,
+          fontWeight: 500,
+          color: orangeColor
+        }}
+      >
+        Manage Data
+      </Typography>
+    </Stack>
+  );
+};
 
 export const ManageCategoriesPage = () => {
-	return (
-		<>
-			<Box mt={2}>
-				<HeadingSection />
-			</Box>
-			<Box mt={4}>
-				<AddCategorySection module="inventory" />
-			</Box>
-			<Box mt={4}>
-				<ManageCategoriesSection module="inventory" />
-			</Box>
-		</>
-	);
+  return (
+    <Box px={2}>
+      <Box mt={2}>
+        <Heading />
+      </Box>
+      <Box mt={4}>
+        <AddCategorySection module="inventory" />
+      </Box>
+      <Box mt={4}>
+        <ManageCategoriesSection module="inventory" />
+      </Box>
+    </Box>
+  );
 };
