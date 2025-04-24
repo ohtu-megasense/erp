57502d6790248914b66be49f57251c40d4786a54
Elias Tuhola <elias.tuhola@gmail.com>
2025-04-24 16:20:08 +0300

    Added and changed app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,12 @@ import { ThemeProvider } from './components/theme-provider/ThemeProvider';
 import { Route, Routes } from 'react-router';
 import { DefaultPageLayout } from './components/pages/layout/DefaultPageLayout';
 import { Redirect } from './components/redirect/Redirect';
-import { VisualizeCategoriesPage } from './components/pages/categories/visualize/VisualizeCategoriesPage';
 import { ManageCategoriesPage } from './components/pages/categories/manage/ManageCategoriesPage';
 import { CRMpage } from './components/pages/categories/CRM/CRMpage';
 import { NotificationSnackbar } from './components/notification/NotificationSnackbar';
 import { DashboardPage } from './components/pages/dashboard/DashboardPage';
 import { ViewPage } from './components/view/ViewPage';
+import { BuildViewPage } from './components/view/BuildViewPage';
 
 export const App = () => {
   return (
@@ -17,17 +17,30 @@ export const App = () => {
       <NotificationSnackbar />
       <Routes>
         <Route element={<DefaultPageLayout />}>
+          {/** Home */}
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/view" element={<ViewPage />} />
+
+          {/** AI chat */}
+          <Route path="/ai-chat" element={null} />
+
+          {/** Inventory pages */}
+          <Route path="/inventory/data" element={<ManageCategoriesPage />} />
           <Route
-            path="/categories/visualize"
-            element={<VisualizeCategoriesPage />}
+            path="/inventory/views"
+            element={<ViewPage module="inventory" />}
           />
           <Route
-            path="/categories/inventory"
-            element={<ManageCategoriesPage />}
+            path="/inventory/views/build"
+            element={<BuildViewPage module="inventory" />}
           />
-          <Route path="/categories/CRM" element={<CRMpage />} />
+
+          {/** CRM pages */}
+          <Route path="/crm/data" element={<CRMpage />} />
+          <Route path="/crm/views" element={<ViewPage module="crm" />} />
+          <Route
+            path="/crm/views/build"
+            element={<BuildViewPage module="crm" />}
+          />
         </Route>
         <Route path="*" element={<Redirect to="/" />} />
       </Routes>
